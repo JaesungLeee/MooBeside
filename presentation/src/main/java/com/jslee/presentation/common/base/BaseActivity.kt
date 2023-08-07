332ef8764f9d19d9b870dc49d5a6bb6c903be0d3
Jaesung Lee <viki0114@naver.com>
2023-08-08 00:16:08 +0900

    #8 add BaseActivity, BaseFragment

---
+++ presentation/src/main/java/com/jslee/presentation/common/base/BaseActivity.kt
@@ -0,0 +1,28 @@
+package com.jslee.presentation.common.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+/**
+ * MooBeside
+ * @author jaesung
+ * @created 2023/08/08
+ */
+open class BaseActivity<VB : ViewDataBinding>(@LayoutRes private val layoutId: Int) :
+    AppCompatActivity() {
+
+    lateinit var binding: VB
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutId)
+
+        initViews()
+    }
+
+    /* must implement */
+    open fun initViews() {}
+}
