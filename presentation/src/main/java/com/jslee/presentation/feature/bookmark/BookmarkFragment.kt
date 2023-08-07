8bdf7e5ead57511094021f3c52c07da94264d1a6
Jaesung Lee <viki0114@naver.com>
2023-08-08 00:51:46 +0900

    #8 add fragments

---
+++ presentation/src/main/java/com/jslee/presentation/feature/bookmark/BookmarkFragment.kt
@@ -0,0 +1,18 @@
+package com.jslee.presentation.feature.bookmark
+
+import com.jslee.presentation.R
+import com.jslee.presentation.common.base.BaseFragment
+import com.jslee.presentation.databinding.FragmentBookmarkBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+/**
+ * MooBeside
+ * @author jaesung
+ * @created 2023/08/08
+ */
+@AndroidEntryPoint
+class BookmarkFragment : BaseFragment<FragmentBookmarkBinding>(R.layout.fragment_bookmark) {
+    override fun initViews() {
+
+    }
+}
---
+++ presentation/src/main/java/com/jslee/presentation/feature/search/SearchFragment.kt
@@ -0,0 +1,18 @@
+package com.jslee.presentation.feature.search
+
+import com.jslee.presentation.R
+import com.jslee.presentation.common.base.BaseFragment
+import com.jslee.presentation.databinding.FragmentSearchBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+/**
+ * MooBeside
+ * @author jaesung
+ * @created 2023/08/08
+ */
+@AndroidEntryPoint
+class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+    override fun initViews() {
+
+    }
+}
