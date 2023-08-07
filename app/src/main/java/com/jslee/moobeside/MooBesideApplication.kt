a9822e433b4919c09a34565a0300d8c15ce57a3b
Jaesung Lee <viki0114@naver.com>
2023-08-07 18:17:51 +0900

    #4 add custom debugging tree for timber

--- app/src/main/java/com/jslee/moobeside/MooBesideApplication.kt
+++ app/src/main/java/com/jslee/moobeside/MooBesideApplication.kt
@@ -1,11 +1,23 @@
 package com.jslee.moobeside
 
 import android.app.Application
+import com.jslee.moobeside.util.CustomTimberDebugTree
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+import javax.inject.Inject
 
 /**
  * MooBeside
  * @author jaesung
  * @created 2023/08/07
  */
+@HiltAndroidApp
 class MooBesideApplication : Application() {
+
+    @Inject
+    lateinit var customDebugTree: CustomTimberDebugTree
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(customDebugTree)
+    }
 }
