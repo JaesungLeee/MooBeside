57ccbb4a8875a40732d6af99ef0e3d5859487db6
Jaesung Lee <viki0114@naver.com>
2023-08-07 21:20:01 +0900

    #4 add AndroidEntryPoint

--- presentation/src/main/java/com/jslee/presentation/feature/MainActivity.kt
+++ presentation/src/main/java/com/jslee/presentation/feature/MainActivity.kt
@@ -3,7 +3,9 @@ package com.jslee.presentation.feature
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jslee.presentation.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
