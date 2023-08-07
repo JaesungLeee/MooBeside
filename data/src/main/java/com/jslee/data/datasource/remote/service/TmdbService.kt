b0061ad63f01ddd3193a044abf424f832b380ee6
Jaesung Lee <viki0114@naver.com>
2023-08-07 18:19:00 +0900

    #4 setup service module

---
+++ data/src/main/java/com/jslee/data/datasource/remote/service/TmdbService.kt
@@ -0,0 +1,9 @@
+package com.jslee.data.datasource.remote.service
+
+/**
+ * MooBeside
+ * @author jaesung
+ * @created 2023/08/07
+ */
+interface TmdbService {
+}
--- data/src/main/java/com/jslee/data/di/ServiceModule.kt
+++ data/src/main/java/com/jslee/data/di/ServiceModule.kt
@@ -1,9 +1,41 @@
 package com.jslee.data.di
 
+import com.jslee.data.datasource.remote.service.KobisService
+import com.jslee.data.datasource.remote.service.TmdbService
+import com.jslee.data.datasource.remote.service.YoutubeService
+import com.jslee.data.di.qualifier.KobisQualifer
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.create
+import javax.inject.Singleton
+
 /**
  * MooBeside
  * @author jaesung
  * @created 2023/08/07
  */
+@Module
+@InstallIn(SingletonComponent::class)
 class ServiceModule {
+
+    @Provides
+    @Singleton
+    fun provideKobisService(
+        @KobisQualifer retrofit: Retrofit
+    ): KobisService = retrofit.create()
+
+    @Provides
+    @Singleton
+    fun provideTmdbService(
+        @KobisQualifer retrofit: Retrofit
+    ): TmdbService = retrofit.create()
+
+    @Provides
+    @Singleton
+    fun provideYoutubeService(
+        @KobisQualifer retrofit: Retrofit
+    ): YoutubeService = retrofit.create()
 }
