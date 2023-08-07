c2d73e807c80a85334840f798a57923d2030355d
Jaesung Lee <viki0114@naver.com>
2023-08-08 02:19:26 +0900

    #8 setup bottom navigation view with nav graph

--- presentation/src/main/java/com/jslee/presentation/feature/MainActivity.kt
+++ presentation/src/main/java/com/jslee/presentation/feature/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.jslee.presentation.feature
 
+import android.view.View
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.jslee.presentation.R
 import com.jslee.presentation.common.base.BaseActivity
 import com.jslee.presentation.databinding.ActivityMainBinding
@@ -7,6 +10,27 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
-    
+    private val navHostFragment by lazy {
+        supportFragmentManager.findFragmentById(binding.fcvMain.id) as NavHostFragment
+    }
 
+    private val navController by lazy { navHostFragment.navController }
+
+    override fun initViews() {
+        setDestinationChangeListener()
+        initBottomNavigationView()
+    }
+
+    private fun setDestinationChangeListener() {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bnvMain.visibility = when (destination.id) {
+                R.id.screeningFragment, R.id.boxOfficeFragment, R.id.bookmarkFragment -> View.VISIBLE
+                else -> View.GONE
+            }
+        }
+    }
+
+    private fun initBottomNavigationView() {
+        binding.bnvMain.setupWithNavController(navController)
+    }
 }
--- presentation/src/main/java/com/jslee/presentation/feature/boxoffice/BoxOfficeFragment.kt
+++ presentation/src/main/java/com/jslee/presentation/feature/boxoffice/BoxOfficeFragment.kt
@@ -1,8 +1,8 @@
-package com.jslee.presentation.feature.settings
+package com.jslee.presentation.feature.boxoffice
 
 import com.jslee.presentation.R
 import com.jslee.presentation.common.base.BaseFragment
-import com.jslee.presentation.databinding.FragmentSettingsBinding
+import com.jslee.presentation.databinding.FragmentBoxOfficeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
@@ -11,7 +11,7 @@ import dagger.hilt.android.AndroidEntryPoint
  * @created 2023/08/08
  */
 @AndroidEntryPoint
-class SettingsFragment : BaseFragment<FragmentSettingsBinding>(R.layout.fragment_settings) {
+class BoxOfficeFragment : BaseFragment<FragmentBoxOfficeBinding>(R.layout.fragment_box_office) {
     override fun initViews() {
 
     }
