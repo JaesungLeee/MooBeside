36b0ab25224daf36885b3ece50d3ab8b794b3d50
Jaesung Lee <viki0114@naver.com>
2023-08-09 03:14:17 +0900

    #10 show date picker

--- presentation/src/main/java/com/jslee/presentation/feature/boxoffice/BoxOfficeFragment.kt
+++ presentation/src/main/java/com/jslee/presentation/feature/boxoffice/BoxOfficeFragment.kt
@@ -1,12 +1,18 @@
 package com.jslee.presentation.feature.boxoffice
 
 import android.widget.Toast
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointBackward
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.jslee.core.date.getDisplayedDate
 import com.jslee.core.date.getDisplayedDateWithDay
+import com.jslee.core.date.getMillisOfPreviousDay
 import com.jslee.presentation.R
 import com.jslee.presentation.common.base.BaseFragment
 import com.jslee.presentation.databinding.FragmentBoxOfficeBinding
 import com.jslee.presentation.feature.boxoffice.adapter.BoxOfficeAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 import javax.inject.Inject
 
 /**
@@ -33,7 +39,11 @@ class BoxOfficeFragment : BaseFragment<FragmentBoxOfficeBinding>(R.layout.fragme
 
     private fun initDatePicker() {
         with(binding) {
-            tvDate.text = System.currentTimeMillis().getDisplayedDateWithDay()
+            tvDate.text =
+                System.currentTimeMillis().getMillisOfPreviousDay().getDisplayedDateWithDay()
+            ivCalendar.setOnClickListener {
+                showDatePickerDialog()
+            }
         }
     }
 
@@ -44,5 +54,25 @@ class BoxOfficeFragment : BaseFragment<FragmentBoxOfficeBinding>(R.layout.fragme
             }
             setHasFixedSize(true)
         }
+    }
+
+    private fun showDatePickerDialog() {
+        val calendarConstraints = CalendarConstraints.Builder()
+            .setValidator(DateValidatorPointBackward.before(System.currentTimeMillis()))
+            .build()
+
+        val datePickerBuilder = MaterialDatePicker.Builder.datePicker().apply {
+            setTitleText("당일은 상영 정보가 제공되지 않습니다.")
+            setCalendarConstraints(calendarConstraints)
+
+        }.build()
+        datePickerBuilder.show(childFragmentManager, DATE_PICKER_TAG)
+        datePickerBuilder.addOnPositiveButtonClickListener {
+            binding.tvDate.text = it.getDisplayedDateWithDay()
+        }
+    }
+
+    companion object {
+        private const val DATE_PICKER_TAG = "DatePicker"
     }
 }
