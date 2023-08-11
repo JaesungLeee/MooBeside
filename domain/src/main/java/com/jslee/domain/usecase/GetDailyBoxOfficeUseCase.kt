7be3f5d8494c7714abc557946cabf689d47d3d18
Jaesung Lee <viki0114@naver.com>
2023-08-11 16:58:59 +0900

    #12 add box office usecase

---
+++ domain/src/main/java/com/jslee/domain/usecase/GetDailyBoxOfficeUseCase.kt
@@ -0,0 +1,26 @@
+package com.jslee.domain.usecase
+
+import kotlinx.coroutines.flow.flow
+import com.jslee.domain.model.Movie
+import com.jslee.domain.repository.MovieRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+/**
+ * MooBeside
+ * @author jaesung
+ * @created 2023/08/11
+ */
+class GetDailyBoxOfficeUseCase @Inject constructor(
+    private val movieRepository: MovieRepository
+) {
+    operator fun invoke(targetDate: String): Flow<List<Movie>> = flow {
+        movieRepository.getDailyBoxOffice(targetDate).collect { result ->
+            result.onSuccess {
+                emit(it)
+            }.onFailure {
+                throw Exception(it.message)
+            }
+        }
+    }
+}
