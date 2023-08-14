9b05c4f055de53959296a44dcf362a6276e827f6
Jaesung Lee <viki0114@naver.com>
2023-08-14 17:21:55 +0900

    #16 add movie search usecase

---
+++ domain/src/main/java/com/jslee/domain/usecase/SearchMovieUseCase.kt
@@ -0,0 +1,27 @@
+package com.jslee.domain.usecase
+
+import com.jslee.domain.model.Movie
+import com.jslee.domain.repository.MovieRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import javax.inject.Inject
+
+/**
+ * MooBeside
+ * @author jaesung
+ * @created 2023/08/14
+ */
+class SearchMovieUseCase @Inject constructor(
+    private val movieRepository: MovieRepository
+) {
+
+    operator fun invoke(query: String): Flow<List<Movie>> = flow {
+        val movieSearchResult = fetchSearchMovieResult(query)
+//        val movieDetailResult = fetchMovieDetailResult()
+        
+    }
+
+    private fun fetchSearchMovieResult(query: String) = movieRepository.getSearchMovie(query = query)
+
+}
