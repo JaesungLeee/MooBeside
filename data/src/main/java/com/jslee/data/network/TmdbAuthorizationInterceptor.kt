d509c9f708f898faf7ad61fa6d0b5ceaee134385
Jaesung Lee <viki0114@naver.com>
2023-08-14 14:17:18 +0900

    #16 add tmdb authorization interceptor

--- data/src/main/java/com/jslee/data/network/TmdbAuthorizationInterceptor.kt
+++ data/src/main/java/com/jslee/data/network/TmdbAuthorizationInterceptor.kt
@@ -1,5 +1,6 @@
 package com.jslee.data.network
 
+import com.jslee.data.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 import javax.inject.Inject
@@ -13,6 +14,14 @@ import javax.inject.Singleton
 @Singleton
 class TmdbAuthorizationInterceptor @Inject constructor(): Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val requestBuilder = request.newBuilder().apply {
+            addHeader(AUTHORIZATION_KEY, "Bearer ${BuildConfig.TMDB_API_KEY}")
+        }
+        return chain.proceed(requestBuilder.build())
+    }
 
+    companion object {
+        private const val AUTHORIZATION_KEY = "Authorization"
     }
 }
