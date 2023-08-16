d93acd974853c2646422f837311b1640e625adda
Jaesung Lee <viki0114@naver.com>
2023-08-16 18:28:32 +0900

    #18 implement instant search

--- presentation/src/main/java/com/jslee/presentation/common/extension/BindingAdapter.kt
+++ presentation/src/main/java/com/jslee/presentation/common/extension/BindingAdapter.kt
@@ -5,6 +5,7 @@ import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 import com.jslee.core.designsystem.R as DR
 import com.jslee.presentation.R
 
@@ -55,5 +56,18 @@ object BindingAdapter {
                 setColorFilter(ContextCompat.getColor(this.context, DR.color.Green))
             }
         } else setImageDrawable(null)
+    }
+
+    @JvmStatic
+    @BindingAdapter("posterImage")
+    fun ImageView.setPosterImage(imageUrl: String?) {
+        if (!imageUrl.isNullOrEmpty()) {
+            Glide.with(this.context)
+                .load(imageUrl)
+                .fitCenter()
+                .override(400, 600)
+                .into(this)
+        } else {
+        }
     }
 }
