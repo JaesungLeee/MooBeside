632dfa3a4091ec556e44d39ce7c4dea987d29a39
Jaesung Lee <viki0114@naver.com>
2023-08-28 15:27:15 +0900

    #27 modify paging source can run on IO dispatcher

--- data/src/main/java/com/jslee/data/paging/base/MooBesidePagingSource.kt
+++ data/src/main/java/com/jslee/data/paging/base/MooBesidePagingSource.kt
@@ -3,7 +3,9 @@ package com.jslee.data.paging.base
 import android.util.Log
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 import kotlin.coroutines.coroutineContext
 
@@ -22,31 +24,30 @@ internal class MooBesidePagingSource<T : Any>(
         }
     }
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, T> {
-        val page = params.key ?: INITIAL_PAGE
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, T> =
+        withContext(Dispatchers.IO) {
+            val page = params.key ?: INITIAL_PAGE
 
-        val context = coroutineContext
-        Log.e("DISPATCHER", context.toString())
+            val context = coroutineContext
+            Log.e("DISPATCHER", context.toString())
 
-        if (page != INITIAL_PAGE) delay(300L)
+            if (page != INITIAL_PAGE) delay(300L)
 
-        return try {
-            val result = executor.invoke(page)
+            try {
+                val result = executor.invoke(page)
 
-            LoadResult.Page(
-                data = result,
-                prevKey = if (page == INITIAL_PAGE) null else page - 1,
-                nextKey = if (result.isEmpty()) null else page + 1,
-            )
+                LoadResult.Page(
+                    data = result,
+                    prevKey = if (page == INITIAL_PAGE) null else page - 1,
+                    nextKey = if (result.isEmpty()) null else page + 1,
+                )
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                throw e
+            } catch (e: Throwable) {
+                LoadResult.Error(e)
+            }
         }
-        catch (e: HttpException) {
-            e.printStackTrace()
-            throw e
-        }
-        catch (e: Throwable) {
-            LoadResult.Error(e)
-        }
-    }
 
     companion object {
         private const val INITIAL_PAGE = 1
