f91ecc426872c100810b54f1a72eced0a22aade4
Jaesung Lee <viki0114@naver.com>
2023-09-08 17:15:16 +0900

    #34 combine popular, now playing, upcoming data

--- presentation/src/main/java/com/jslee/presentation/feature/home/HomeFragment.kt
+++ presentation/src/main/java/com/jslee/presentation/feature/home/HomeFragment.kt
@@ -2,11 +2,9 @@ package com.jslee.presentation.feature.home
 
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import androidx.viewpager2.widget.ViewPager2.SCROLL_STATE_DRAGGING
-import androidx.viewpager2.widget.ViewPager2.SCROLL_STATE_IDLE
 import com.jslee.core.designsystem.tooltip.builder.TooltipBuilder
 import com.jslee.core.ui.base.view.BaseFragment
 import com.jslee.presentation.R
@@ -14,9 +12,9 @@ import com.jslee.presentation.databinding.FragmentHomeBinding
 import com.jslee.presentation.feature.home.adapter.HomeBannerAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 import com.jslee.core.designsystem.R as DR
 
@@ -35,27 +33,27 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     private lateinit var autoScrollJob: Job
     private var currentPosition = 0
 
-    override fun onResume() {
-        super.onResume()
-        createAutoScrollJob()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        autoScrollJob.cancel()
-    }
-
-    private fun createAutoScrollJob() {
-        autoScrollJob = viewLifecycleOwner.lifecycleScope.launch {
-            delay(3000L)
-            binding.vpPopularBanner.setCurrentItem(++currentPosition, true)
-        }
-    }
+//    override fun onResume() {
+//        super.onResume()
+//        createAutoScrollJob()
+//    }
+//
+//    override fun onPause() {
+//        super.onPause()
+//        autoScrollJob.cancel()
+//    }
+//
+//    private fun createAutoScrollJob() {
+//        autoScrollJob = viewLifecycleOwner.lifecycleScope.launch {
+//            delay(3000L)
+//            binding.vpPopularBanner.setCurrentItem(++currentPosition, true)
+//        }
+//    }
 
     override fun initViews() {
-        initToolbarMenu()
-        initTopBanner()
-        initTooltip()
+//        initToolbarMenu()
+//        initTopBanner()
+//        initTooltip()
     }
 
     private fun initToolbarMenu() {
@@ -83,7 +81,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
                 override fun onPageScrollStateChanged(state: Int) {
                     super.onPageScrollStateChanged(state)
                     when (state) {
-                        SCROLL_STATE_IDLE -> createAutoScrollJob()
+//                        SCROLL_STATE_IDLE -> createAutoScrollJob()
                         SCROLL_STATE_DRAGGING -> autoScrollJob.cancel()
                     }
                 }
@@ -105,11 +103,36 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
     override fun observeStates() {
         repeatOn(Lifecycle.State.STARTED) {
-            viewModel.banner.collectLatest { bannerList ->
-                binding.vpPopularBanner.adapter = bannerAdapter.also {
-                    it.submitList(bannerList)
+//            launch {
+//                viewModel.banner.collectLatest { bannerList ->
+//                    binding.vpPopularBanner.adapter = bannerAdapter.also {
+//                        it.submitList(bannerList)
+//                    }
+//                }
+//            }
+
+            launch {
+                viewModel.homeUiState.collectLatest { uiState ->
+                    handleState(uiState)
+                }
+            }
+
+            launch {
+                viewModel.errorFlow.collectLatest { throwable ->
+                    handleException(throwable)
                 }
             }
         }
+    }
+
+    private fun handleState(uiState: HomeUiState) = when (uiState) {
+        is HomeUiState.Loading -> {}
+        is HomeUiState.Success -> {
+            Timber.e("${uiState.data}")
+        }
+    }
+
+    private fun handleException(exception: Throwable) {
+
     }
 }
--- presentation/src/main/java/com/jslee/presentation/feature/home/HomeViewModel.kt
+++ presentation/src/main/java/com/jslee/presentation/feature/home/HomeViewModel.kt
@@ -2,13 +2,27 @@ package com.jslee.presentation.feature.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.jslee.domain.usecase.GetNowPlayingMovieUseCase
 import com.jslee.domain.usecase.GetPopularMovieUseCase
 import com.jslee.presentation.feature.home.model.BannerUiModel
+import com.jslee.domain.usecase.GetUpComingMovieUseCase
+import com.jslee.presentation.feature.home.model.item.HomeListItem
+import com.jslee.presentation.feature.home.model.mapToBannerUiModel
+import com.jslee.presentation.feature.home.model.mapToNowPlayingUiModel
+import com.jslee.presentation.feature.home.model.mapToUpComingUiModel
 import com.jslee.presentation.feature.home.model.toBannerUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 /**
@@ -18,14 +32,69 @@ import javax.inject.Inject
  */
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    getPopularMovieUseCase: GetPopularMovieUseCase
+    getPopularMovieUseCase: GetPopularMovieUseCase,
+    getNowPlayingMovieUseCase: GetNowPlayingMovieUseCase,
+    getUpcomingMovieUseCase: GetUpComingMovieUseCase,
 ) : ViewModel() {
+
+    private val _homeUiState: MutableStateFlow<HomeUiState> = MutableStateFlow(HomeUiState.Loading)
+    val homeUiState = _homeUiState.asStateFlow()
+
+    private val _errorFlow: MutableSharedFlow<Throwable> = MutableSharedFlow<Throwable>()
+    val errorFlow = _errorFlow.asSharedFlow()
 
     val banner = getPopularMovieUseCase.invoke().map { movieList ->
         movieList.map { it.toBannerUiModel() }
     }.stateIn(
         viewModelScope,
-        SharingStarted.Eagerly,
+        SharingStarted.WhileSubscribed(5000L),
         List(BANNER_COUNT) { BannerUiModel.default() }
     )
+
+    private val popularMoviesFlow: Flow<List<HomeListItem>> =
+        getPopularMovieUseCase().map { it.mapToBannerUiModel() }
+
+    private val nowPlayingMoviesFlow: Flow<List<HomeListItem>> =
+        getNowPlayingMovieUseCase.getSnapshot().map { it.mapToNowPlayingUiModel() }
+
+    private val upComingMoviesFlow: Flow<List<HomeListItem>> =
+        getUpcomingMovieUseCase.getSnapshot().map { it.mapToUpComingUiModel() }
+
+
+//    val nowPlayingMovies = getNowPlayingMovieUseCase.getSnapshot().map { movieList ->
+//        movieList.map { it.toNowPlayingUiModel() }
+//    }.stateIn(
+//        viewModelScope,
+//        SharingStarted.WhileSubscribed(5000L),
+//        emptyList()
+//    )
+
+//    val upComingMovies = getUpcomingMovieUseCase.getSnapshot().map { movieList ->
+//        movieList.map { it.toUpComingUiModel() }
+//    }.stateIn(
+//        viewModelScope,
+//        SharingStarted.WhileSubscribed(5000L),
+//        emptyList()
+//    )
+
+    init {
+        viewModelScope.launch {
+            combine(
+                popularMoviesFlow,
+                nowPlayingMoviesFlow,
+                upComingMoviesFlow,
+            ) { bannerList, nowPlayingList, upComingList ->
+                HomeUiState.Success(bannerList + nowPlayingList + upComingList)
+            }.catch {
+                _errorFlow.emit(it)
+            }.collect {
+                _homeUiState.value = it
+            }
+        }
+    }
+}
+
+sealed class HomeUiState {
+    object Loading : HomeUiState()
+    data class Success(val data: List<HomeListItem>) : HomeUiState()
 }
