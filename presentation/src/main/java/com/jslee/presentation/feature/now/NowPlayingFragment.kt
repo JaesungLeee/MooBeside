5aac63a07e63d7a0801833904ec38e0cf72ebe99
Jaesung Lee <viki0114@naver.com>
2023-09-15 01:37:29 +0900

    #42 implement now playing movie with paging data

--- presentation/src/main/java/com/jslee/presentation/feature/now/NowPlayingFragment.kt
+++ presentation/src/main/java/com/jslee/presentation/feature/now/NowPlayingFragment.kt
@@ -1,10 +1,17 @@
 package com.jslee.presentation.feature.now
 
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import com.jslee.core.ui.base.view.BaseFragment
+import com.jslee.core.ui.decoration.CommonItemDecoration
+import com.jslee.core.ui.decoration.LayoutType
+import com.jslee.core.ui.model.PaddingValues
 import com.jslee.presentation.R
 import com.jslee.presentation.databinding.FragmentNowPlayingBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 /**
  * MooBeside
@@ -14,13 +21,53 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class NowPlayingFragment :
     BaseFragment<FragmentNowPlayingBinding>(R.layout.fragment_now_playing) {
+
+    private val viewModel: NowPlayingViewModel by viewModels()
+    private val nowPlayingAdapter: NowPlayingPagingAdapter by lazy { NowPlayingPagingAdapter() }
     override fun initViews() {
         initToolbar()
+        initRecyclerView()
     }
 
     private fun initToolbar() {
         binding.tbNowPlaying.setOnClickListener {
             findNavController().navigateUp()
         }
+    }
+
+    private fun initRecyclerView() = with(binding.rvNowPlaying) {
+        adapter = nowPlayingAdapter
+        val paddingValues = PaddingValues(2, 2, 2, 2)
+        addItemDecoration(CommonItemDecoration(paddingValues, LayoutType.GRID))
+    }
+
+    override fun observeStates() {
+        repeatOn(Lifecycle.State.STARTED) {
+            launch {
+                viewModel.nowPlayingUiState.collectLatest {
+                    handleUiState(it)
+                }
+            }
+
+            launch {
+                viewModel.eventFlow.collect {
+                    handleException(it)
+                }
+            }
+        }
+    }
+
+    private suspend fun handleUiState(uiState: NowPlayingUiState) = when (uiState) {
+        Loading -> {
+
+        }
+
+        is Success -> {
+            nowPlayingAdapter.submitData(uiState.data)
+        }
+    }
+
+    private fun handleException(throwable: Throwable) {
+
     }
 }
---
+++ presentation/src/main/java/com/jslee/presentation/feature/now/NowPlayingPagingAdapter.kt
@@ -0,0 +1,47 @@
+package com.jslee.presentation.feature.now
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import com.jslee.presentation.databinding.ItemNowPlayingPosterBinding
+import com.jslee.presentation.feature.home.model.NowPlayingUiModel
+import com.jslee.presentation.feature.home.viewholder.nowplaying.NowPlayingPosterViewHolder
+
+/**
+ * MooBeside
+ * @author jaesung
+ * @created 2023/09/15
+ */
+class NowPlayingPagingAdapter() :
+    PagingDataAdapter<NowPlayingUiModel, NowPlayingPosterViewHolder>(NOW_PLAYING_DIFF_CALLBACK) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NowPlayingPosterViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemNowPlayingPosterBinding.inflate(layoutInflater, parent, false)
+        return NowPlayingPosterViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: NowPlayingPosterViewHolder, position: Int) {
+        val item = getItem(position) ?: return
+        holder.bindItems(item)
+    }
+
+    companion object {
+        private val NOW_PLAYING_DIFF_CALLBACK =
+            object : DiffUtil.ItemCallback<NowPlayingUiModel>() {
+                override fun areItemsTheSame(
+                    oldItem: NowPlayingUiModel,
+                    newItem: NowPlayingUiModel
+                ): Boolean {
+                    return oldItem.movieId == newItem.movieId
+                }
+
+                override fun areContentsTheSame(
+                    oldItem: NowPlayingUiModel,
+                    newItem: NowPlayingUiModel
+                ): Boolean {
+                    return oldItem == newItem
+                }
+            }
+    }
+}
