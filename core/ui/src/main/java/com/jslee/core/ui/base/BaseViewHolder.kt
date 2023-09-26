7709c995dfd9c4768f41285910232c23231e7de2
Jaesung Lee <viki0114@naver.com>
2023-09-26 16:39:45 +0900

    #48 add getItem base abstraction

--- core/ui/src/main/java/com/jslee/core/ui/base/BaseViewHolder.kt
+++ core/ui/src/main/java/com/jslee/core/ui/base/BaseViewHolder.kt
@@ -1,6 +1,7 @@
 package com.jslee.core.ui.base
 
 import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
 /**
@@ -9,9 +10,33 @@ import androidx.recyclerview.widget.RecyclerView
  * @created 2023/09/01
  */
 abstract class BaseViewHolder<out T>(
-    binding: ViewDataBinding
+    binding: ViewDataBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
 
     /* must implement */
     open fun bindItems(item: @UnsafeVariance T) {}
+
+    /**
+     * https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/-published-api/
+     *
+     * internal에 의한 non-public api를 public api inline에서 사용 가능하도록 허용
+     */
+    @PublishedApi
+    internal fun getItem(position: Int): Any {
+        return when (val adapter = bindingAdapter) {
+            is ListAdapter<*, *> -> {
+                adapter.currentList[position]
+            }
+
+            else -> {
+                throw IllegalAccessException()
+            }
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    inline fun getItem(action: (item: T) -> Unit) {
+        val item = getItem(bindingAdapterPosition) as T
+        action(item)
+    }
 }
