fb5fdf8b972ed0642d515881d1b74931635b8bbd
Jaesung Lee <viki0114@naver.com>
2023-10-03 14:35:45 +0900

    #52 add release date mapper

--- data/src/main/java/com/jslee/data/datasource/remote/dto/response/tmdb/TmdbReleaseDateResponse.kt
+++ data/src/main/java/com/jslee/data/datasource/remote/dto/response/tmdb/TmdbReleaseDateResponse.kt
@@ -1,5 +1,7 @@
 package com.jslee.data.datasource.remote.dto.response.tmdb
 
+import com.jslee.data.UNKNOWN_FIELD
+import com.jslee.data.model.ReleaseDateMovieModel
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -22,4 +24,10 @@ data class ReleaseDateResponse(
     @SerialName("note") val note: String,
     @SerialName("release_date") val releaseDate: String,
     @SerialName("type") val releaseType: Int,
+)
+
+internal fun TmdbReleaseDateResponse.toDataModel() = ReleaseDateMovieModel(
+    regionCode = regionCode,
+    certification = releaseDate.map { it.certification }.firstOrNull() ?: UNKNOWN_FIELD,
+    releaseDate = releaseDate.map { it.releaseDate }.firstOrNull() ?: UNKNOWN_FIELD
 )
