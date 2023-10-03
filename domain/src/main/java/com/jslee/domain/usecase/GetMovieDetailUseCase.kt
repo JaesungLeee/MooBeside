2c56652bb1d7227b4e37c11383733279beb73efa
Jaesung Lee <viki0114@naver.com>
2023-10-03 17:16:37 +0900

    #52 combine release date with credits

--- domain/src/main/java/com/jslee/domain/usecase/GetMovieDetailUseCase.kt
+++ domain/src/main/java/com/jslee/domain/usecase/GetMovieDetailUseCase.kt
@@ -1,6 +1,8 @@
 package com.jslee.domain.usecase
 
+import com.jslee.domain.model.Movie
 import com.jslee.domain.repository.MovieRepository
+import kotlinx.coroutines.flow.combine
 import javax.inject.Inject
 
 /**
@@ -9,10 +11,19 @@ import javax.inject.Inject
  * @created 2023/10/03
  */
 class GetMovieDetailUseCase @Inject constructor(
-    private val movieRepository: MovieRepository
+    private val movieRepository: MovieRepository,
 ) {
 
     private fun getMovieReleaseDate(movieId: Long) = movieRepository.getMovieReleaseDate(movieId)
+    private fun getMovieCredits(movieId: Long) = movieRepository.getMovieCredits(movieId)
 
-    operator fun invoke(movieId: Long) = getMovieReleaseDate(movieId)
+    operator fun invoke(movieId: Long) =
+        combine(getMovieReleaseDate(movieId), getMovieCredits(movieId)) { releaseDates, credits ->
+            Movie(
+                releaseDate = releaseDates.releaseDate,
+                certification = releaseDates.certification,
+                casts = credits.casts,
+                staffs = credits.staffs
+            )
+        }
 }
