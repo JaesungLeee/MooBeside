80e2f7a6a029e1378d078ed8048f8bccd001b3c1
Jaesung Lee <viki0114@naver.com>
2023-10-24 16:50:40 +0900

    #61 bind data with movie info nested recyclerview

--- presentation/src/main/java/com/jslee/presentation/feature/detail/viewholder/info/MovieInfoViewHolder.kt
+++ presentation/src/main/java/com/jslee/presentation/feature/detail/viewholder/info/MovieInfoViewHolder.kt
@@ -1,7 +1,9 @@
 package com.jslee.presentation.feature.detail.viewholder.info
 
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
 import com.jslee.core.ui.base.BaseViewHolder
 import com.jslee.presentation.databinding.ItemDetailInfoBinding
+import com.jslee.presentation.feature.detail.adapter.info.MovieInfoListAdapter
 import com.jslee.presentation.feature.detail.model.item.DetailListItem
 
 /**
@@ -12,8 +14,21 @@ import com.jslee.presentation.feature.detail.model.item.DetailListItem
 class MovieInfoViewHolder(
     private val binding: ItemDetailInfoBinding,
 ) : BaseViewHolder<DetailListItem.MovieInfo>(binding) {
+
+    private val movieInfoListAdapter = MovieInfoListAdapter()
+
+    init {
+        initMovieInfoList()
+    }
+
+    private fun initMovieInfoList() = with(binding.rvMovieInfo) {
+        adapter = movieInfoListAdapter
+//        addItemDecoration()
+    }
+
     override fun bindItems(item: DetailListItem.MovieInfo) = with(binding) {
         movieInfo = item.movieInfoData
+        movieInfoListAdapter.submitList(item.movieInfoData.movieInfoData)
         executePendingBindings()
     }
 }
