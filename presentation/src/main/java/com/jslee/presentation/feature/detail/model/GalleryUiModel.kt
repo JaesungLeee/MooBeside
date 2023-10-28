fdb5ce51d8cffb235b9b64e57756bb53f60056b6
Jaesung Lee <viki0114@naver.com>
2023-10-29 01:14:22 +0900

    #61 map to gallery item

---
+++ presentation/src/main/java/com/jslee/presentation/feature/detail/model/GalleryUiModel.kt
@@ -0,0 +1,16 @@
+package com.jslee.presentation.feature.detail.model
+
+import com.jslee.domain.model.movie.Movie
+
+/**
+ * MooBeside
+ * @author jaesung
+ * @created 2023/10/29
+ */
+data class GalleryUiModel(
+    val galleryImageUrl: String?,
+)
+
+fun Movie.mapToGalleryUiModel(): List<GalleryUiModel> {
+    return images?.map { GalleryUiModel(it) }.orEmpty()
+}
--- presentation/src/main/java/com/jslee/presentation/feature/detail/model/MovieDetailUiModel.kt
+++ presentation/src/main/java/com/jslee/presentation/feature/detail/model/MovieDetailUiModel.kt
@@ -1,5 +1,8 @@
 package com.jslee.presentation.feature.detail.model
 
+import com.jslee.core.ui.extension.makeSummaryInfo
+import com.jslee.domain.model.movie.Movie
+import com.jslee.domain.model.movie.MovieStatus
 import com.jslee.presentation.feature.detail.model.item.DetailListItem
 
 /**
@@ -17,4 +20,33 @@ data class AppBarUiModel(
     val posterImageUrl: String?,
     val backdropImageUrl: String?,
     val summaryInfo: String,
+)
+
+fun Movie.toMovieDetailUiModel() = MovieDetailUiModel(
+    appBarModel = toAppBarModel(),
+    detailData = toListItem()
+)
+
+fun Movie.toAppBarModel() = AppBarUiModel(
+    movieName = localizedMovieName.orEmpty(),
+    posterImageUrl = posterImageUrl,
+    backdropImageUrl = backdropImageUrl,
+    summaryInfo = makeSummaryInfo(
+        localizedReleaseDate,
+        MovieStatus.getDescription(movieStatus),
+        genres
+    ),
+)
+
+const val SCREEN_SHOWN_LIMIT = 4
+fun Movie.toListItem() = listOf(
+    DetailListItem.MovieInfo(
+        movieInfoData = toMovieInfoUiModel()
+    ),
+    DetailListItem.Cast(
+        castInfoData = mapToCastInfoUiModel().take(SCREEN_SHOWN_LIMIT)
+    ),
+    DetailListItem.Gallery(
+        galleryData = mapToGalleryUiModel()
+    )
 )
