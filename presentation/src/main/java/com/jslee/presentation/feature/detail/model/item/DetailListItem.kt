0b1fa0638e1ae14bc4b61fb3b09122ce2aee77ea
Jaesung Lee <viki0114@naver.com>
2023-10-29 01:14:22 +0900

    #61 map to gallery item

--- presentation/src/main/java/com/jslee/presentation/feature/detail/model/item/DetailListItem.kt
+++ presentation/src/main/java/com/jslee/presentation/feature/detail/model/item/DetailListItem.kt
@@ -2,6 +2,7 @@ package com.jslee.presentation.feature.detail.model.item
 
 import com.jslee.core.ui.model.ListItem
 import com.jslee.presentation.feature.detail.model.CastInfoUiModel
+import com.jslee.presentation.feature.detail.model.GalleryUiModel
 import com.jslee.presentation.feature.detail.model.MovieInfoUiModel
 
 /**
@@ -12,18 +13,21 @@ import com.jslee.presentation.feature.detail.model.MovieInfoUiModel
 sealed class DetailListItem(override val viewType: DetailViewType) : ListItem {
 
     enum class DetailViewType {
-        MOVIE_INFO,
-        CAST
+        MOVIE_INFO, CAST, GALLERY;
     }
 
     data class MovieInfo(
         override val id: Long = 0,
-        val movieInfoData: MovieInfoUiModel
+        val movieInfoData: MovieInfoUiModel,
     ) : DetailListItem(DetailViewType.MOVIE_INFO)
 
     data class Cast(
         override val id: Long = 1,
-        val castInfoData: List<CastInfoUiModel>
+        val castInfoData: List<CastInfoUiModel>,
     ) : DetailListItem(DetailViewType.CAST)
 
+    data class Gallery(
+        override val id: Long = 2,
+        val galleryData: List<GalleryUiModel>
+    ) : DetailListItem(DetailViewType.GALLERY)
 }
