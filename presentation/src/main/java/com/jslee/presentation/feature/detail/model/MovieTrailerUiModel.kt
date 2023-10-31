07866392a77507518d99671a08dd751f2bf2c0e9
Jaesung Lee <viki0114@naver.com>
2023-10-31 18:33:04 +0900

    #69 add trailer ui model mapper

---
+++ presentation/src/main/java/com/jslee/presentation/feature/detail/model/MovieTrailerUiModel.kt
@@ -0,0 +1,25 @@
+package com.jslee.presentation.feature.detail.model
+
+import com.jslee.core.ui.extension.toDisplayedDateWithFullFormat
+import com.jslee.domain.model.movie.Movie
+
+/**
+ * MooBeside
+ * @author jaesung
+ * @created 2023/10/31
+ */
+data class MovieTrailerUiModel(
+    val videoId: String,
+    val thumbnailUrl: String,
+    val contentTitle: String,
+    val publishedDate: String,
+)
+
+fun Movie.mapToMovieTrailerUiModel(): List<MovieTrailerUiModel> = trailers?.map {
+    MovieTrailerUiModel(
+        videoId = it.videoId,
+        thumbnailUrl = it.thumbnailUrl,
+        contentTitle = it.contentTitle,
+        publishedDate = it.publishedDate.toDisplayedDateWithFullFormat()
+    )
+}.orEmpty()
