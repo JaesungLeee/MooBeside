47d72d298ec13296c6ac6d56d41a8b039871f3ec
Jaesung Lee <viki0114@naver.com>
2023-10-31 11:11:04 +0900

    #64 add youtube search api service

---
+++ data/src/main/java/com/jslee/data/datasource/remote/dto/response/youtube/YoutubeSearchResponse.kt
@@ -0,0 +1,52 @@
+package com.jslee.data.datasource.remote.dto.response.youtube
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/**
+ * MooBeside
+ * @author jaesung
+ * @created 2023/10/30
+ */
+@Serializable
+internal data class YoutubeSearchResponse(
+    @SerialName("kind") val responseKind: String,
+    @SerialName("etag") val eTag: String,
+    @SerialName("nextPageToken") val nextPageToken: String,
+    @SerialName("regionCode") val regionCode: String,
+    @SerialName("pageInfo") val pageInfo: YoutubePageInfoResponse,
+    @SerialName("items") val responseItem: List<YoutubeSearchItemsResponse>,
+)
+
+@Serializable
+internal data class YoutubePageInfoResponse(
+    @SerialName("totalResults") val totalCount: Int,
+    @SerialName("resultsPerPage") val countPerPage: Int,
+)
+
+@Serializable
+internal data class YoutubeSearchItemsResponse(
+    @SerialName("kind") val responseKind: String,
+    @SerialName("etag") val eTag: String,
+    @SerialName("id") val itemId: YoutubeSearchItemIdResponse,
+    @SerialName("snippet") val item: YoutubeSearchContentResponse,
+)
+
+@Serializable
+internal data class YoutubeSearchItemIdResponse(
+    @SerialName("kind") val responseKind: String,
+    @SerialName("videoId") val videoId: String,
+)
+
+@Serializable
+internal data class YoutubeSearchContentResponse(
+    @SerialName("publishedAt") val publishedDate: String,
+    @SerialName("channelId") val channelId: String,
+    @SerialName("title") val contentTitle: String,
+    @SerialName("description") val description: String,
+    @SerialName("thumbnails") val thumbnails: YoutubeThumbnailResponse,
+    @SerialName("channelTitle") val channelTitle: String,
+    @SerialName("liveBroadcastContent") val liveBroadcastContent: String,
+    @SerialName("publishTime") val publishTime: String,
+)
+
--- data/src/main/java/com/jslee/data/datasource/remote/service/YoutubeService.kt
+++ data/src/main/java/com/jslee/data/datasource/remote/service/YoutubeService.kt
@@ -1,9 +1,29 @@
 package com.jslee.data.datasource.remote.service
 
+import com.jslee.data.BuildConfig
+import com.jslee.data.datasource.remote.dto.response.youtube.YoutubeSearchResponse
+import com.jslee.domain.model.Country
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 /**
  * MooBeside
  * @author jaesung
  * @created 2023/08/07
  */
-class YoutubeService {
+internal interface YoutubeService {
+    @GET("search")
+    suspend fun getVideoList(
+        @Query("key") key: String = BuildConfig.YOUTUBE_API_KEY,
+        @Query("part") part: String = RESPONSE_PART,
+        @Query("maxResults") result: UInt = MAX_COUNT,
+        @Query("q") query: String,
+        @Query("regionCode") regionCode: String = Country.KOREA.regionCode,
+    ): Response<YoutubeSearchResponse>
+
+    companion object {
+        private const val RESPONSE_PART = "snippet"
+        private const val MAX_COUNT = 10u
+    }
 }
