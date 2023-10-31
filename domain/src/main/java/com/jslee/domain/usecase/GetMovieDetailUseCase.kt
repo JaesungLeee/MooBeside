41c0ae3edf396902d5455c5560932f31fc7a56f6
Jaesung Lee <viki0114@naver.com>
2023-10-31 17:01:04 +0900

    #64 add fetching trailers logic in GetMovieDetailUseCase

--- domain/src/main/java/com/jslee/domain/usecase/GetMovieDetailUseCase.kt
+++ domain/src/main/java/com/jslee/domain/usecase/GetMovieDetailUseCase.kt
@@ -1,7 +1,9 @@
 package com.jslee.domain.usecase
 
+import com.jslee.domain.model.Trailer
 import com.jslee.domain.model.movie.Movie
 import com.jslee.domain.repository.MovieRepository
+import com.jslee.domain.repository.TrailerRepository
 import kotlinx.coroutines.flow.combine
 import javax.inject.Inject
 
@@ -12,41 +14,51 @@ import javax.inject.Inject
  */
 class GetMovieDetailUseCase @Inject constructor(
     private val movieRepository: MovieRepository,
+    private val trailerRepository: TrailerRepository,
 ) {
     private fun getMovieReleaseInfo(movieId: Long) = movieRepository.getMovieReleaseInfo(movieId)
     private fun getMovieCredits(movieId: Long) = movieRepository.getMovieCredits(movieId)
     private fun getMovieDetail(movieId: Long) = movieRepository.getMovieDetail(movieId)
     private fun getMovieImages(movieId: Long) = movieRepository.getMovieImages(movieId)
+    private fun getMovieTrailers(query: String) = trailerRepository.getMovieTrailers(query)
 
-    operator fun invoke(movieId: Long) =
-        combine(
-            getMovieDetail(movieId),
-            getMovieReleaseInfo(movieId),
-            getMovieCredits(movieId),
-            getMovieImages(movieId)
-        ) { movieDetail, releaseInfo, credits, posters ->
-            Movie(
-                kobisMovieCode = null,
-                tmdbMovieId = movieDetail.tmdbMovieId,
-                localizedMovieName = movieDetail.localizedMovieName,
-                tagline = movieDetail.tagline,
-                overview = movieDetail.overview,
-                runtime = movieDetail.runtime,
-                movieStatus = movieDetail.movieStatus,
-                genres = movieDetail.genres,
-                originalReleaseDate = movieDetail.originalReleaseDate,
-                localizedReleaseDate = releaseInfo.localizedReleaseDate,
-                spokenLanguage = movieDetail.spokenLanguage,
-                posterImageUrl = movieDetail.posterImageUrl,
-                backdropImageUrl = movieDetail.backdropImageUrl,
-                certification = releaseInfo.certification,
-                isAdultMovie = movieDetail.isAdultMovie,
-                rateInfo = movieDetail.rateInfo,
-                boxOffice = null,
-                images = posters,
-                productionCompanies = movieDetail.productionCompanies,
-                casts = credits.casts,
-                staffs = credits.staffs
-            )
+    /**
+     *  movie trailer 병렬 처리 가능하도록 수정 필요
+     */
+    operator fun invoke(movieId: Long) = combine(
+        getMovieDetail(movieId),
+        getMovieReleaseInfo(movieId),
+        getMovieCredits(movieId),
+        getMovieImages(movieId)
+    ) { movieDetail, releaseInfo, credits, posters ->
+        val trailers = mutableListOf<Trailer>()
+        getMovieTrailers(movieDetail.localizedMovieName.orEmpty()).collect { remoteTrailers ->
+            remoteTrailers.filter { it.isVideoIdEmpty }
+            trailers.addAll(remoteTrailers)
         }
+        Movie(
+            kobisMovieCode = null,
+            tmdbMovieId = movieDetail.tmdbMovieId,
+            localizedMovieName = movieDetail.localizedMovieName,
+            tagline = movieDetail.tagline,
+            overview = movieDetail.overview,
+            runtime = movieDetail.runtime,
+            movieStatus = movieDetail.movieStatus,
+            genres = movieDetail.genres,
+            originalReleaseDate = movieDetail.originalReleaseDate,
+            localizedReleaseDate = releaseInfo.localizedReleaseDate,
+            spokenLanguage = movieDetail.spokenLanguage,
+            posterImageUrl = movieDetail.posterImageUrl,
+            backdropImageUrl = movieDetail.backdropImageUrl,
+            certification = releaseInfo.certification,
+            isAdultMovie = movieDetail.isAdultMovie,
+            rateInfo = movieDetail.rateInfo,
+            boxOffice = null,
+            images = posters,
+            productionCompanies = movieDetail.productionCompanies,
+            casts = credits.casts,
+            staffs = credits.staffs,
+            trailers = trailers
+        )
+    }
 }
