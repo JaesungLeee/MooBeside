0942df0fd3d64e83ec25e2f29103556c4f7c3be4
Jaesung Lee <viki0114@naver.com>
2023-11-06 17:25:06 +0900

    #87 add unknown enum

--- domain/src/main/java/com/jslee/domain/model/movie/Certification.kt
+++ domain/src/main/java/com/jslee/domain/model/movie/Certification.kt
@@ -33,6 +33,11 @@ enum class Certification(val code: String, val meaning: String, val description:
         code = "Restricted Screening",
         meaning = "제한",
         description = "폭력성·선정성·사회적 행위 등의 표현이 과도하여 인간의 보편적 존엄, 사회적 가치, 선량한 풍속 또는 국민 정서를 현저하게 해할 우려가 있어 상영 및 광고·선전에 있어 일정한 제한이 필요한 영화"
+    ),
+    UNKNWON(
+        code = "UNKNOWN",
+        meaning = "확인불가",
+        description = "확인불가"
     );
 
     companion object {
@@ -43,7 +48,7 @@ enum class Certification(val code: String, val meaning: String, val description:
                 R_15.code -> R_15
                 R_18.code -> R_18
                 RESTRICTED.code -> RESTRICTED
-                else -> RESTRICTED
+                else -> UNKNWON
             }
         }
 
@@ -54,7 +59,7 @@ enum class Certification(val code: String, val meaning: String, val description:
                 R_15 -> R_15.meaning
                 R_18 -> R_18.meaning
                 RESTRICTED -> RESTRICTED.meaning
-                else -> throw IllegalArgumentException()
+                else -> UNKNWON.meaning
             }
         }
     }
