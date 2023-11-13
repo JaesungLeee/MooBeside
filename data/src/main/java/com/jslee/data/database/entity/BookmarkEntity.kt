8d8cdb09fe75e8e5ae6c90f3bd8ac29229dc6676
Jaesung Lee <viki0114@naver.com>
2023-11-13 19:24:58 +0900

    #82 add entity mapper with local datasource

--- data/src/main/java/com/jslee/data/database/entity/BookmarkEntity.kt
+++ data/src/main/java/com/jslee/data/database/entity/BookmarkEntity.kt
@@ -3,7 +3,9 @@ package com.jslee.data.database.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.jslee.data.TMDB_IMAGE_PREFIX
 import com.jslee.data.database.MOOBESIDE_BOOKMARK_DATABASE
+import com.jslee.domain.model.movie.Movie
 import com.jslee.domain.model.movie.MovieStatus
 
 /**
@@ -13,12 +15,33 @@ import com.jslee.domain.model.movie.MovieStatus
  */
 @Entity(tableName = MOOBESIDE_BOOKMARK_DATABASE)
 internal data class BookmarkEntity(
-    @PrimaryKey @ColumnInfo("movie_id") val movieId: Long,
-    @ColumnInfo("movie_name") val localizedMovieName: String,
-    @ColumnInfo("poster_url") val posterImageUrl: String,
-    @ColumnInfo("runtime") val runtime: Int,
-    @ColumnInfo("released_year") val movieReleasedYear: String,
-    @ColumnInfo("genres") val genres: List<String>,
-    @ColumnInfo("status") val movieReleaseStatus: String,
+    @PrimaryKey @ColumnInfo("movie_id") val movieId: Long? = 0,
+    @ColumnInfo("movie_name") val movieName: String? = "",
+    @ColumnInfo("poster_url") val posterImageUrl: String? = "",
+    @ColumnInfo("runtime") val runtime: Int? = 0,
+    @ColumnInfo("released_date") val releasedDate: String? = "",
+    @ColumnInfo("genres") val genres: List<String>? = emptyList(),
+    @ColumnInfo("status") val releaseStatus: String? = "",
     @ColumnInfo("bookmarked_at") val bookmarkedAt: Long,
+)
+
+internal fun BookmarkEntity.toDomain() = Movie(
+    tmdbMovieId = movieId,
+    localizedMovieName = movieName,
+    posterImageUrl = TMDB_IMAGE_PREFIX + posterImageUrl,
+    runtime = runtime,
+    localizedReleaseDate = releasedDate,
+    genres = genres,
+    movieStatus = MovieStatus.fromDescription(releaseStatus.orEmpty()),
+)
+
+internal fun Movie.toBookmarkEntity(bookmarkedAt: Long) = BookmarkEntity(
+    movieId = tmdbMovieId ?: -1,
+    movieName = localizedMovieName.orEmpty(),
+    posterImageUrl = posterImageUrl,
+    runtime = runtime,
+    releasedDate = localizedReleaseDate,
+    genres = genres,
+    releaseStatus = MovieStatus.getDescription(movieStatus),
+    bookmarkedAt = bookmarkedAt
 )
