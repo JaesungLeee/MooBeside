74172c9ed0d4c480cc0bfdbe1f3276e6ba4bddf5
Jaesung Lee <viki0114@naver.com>
2023-12-21 19:02:00 +0900

    #101 remove data module targetSdk

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -15,7 +15,6 @@ android {
 
     defaultConfig {
         minSdk = 24
-        targetSdk = 33
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "KOBIS_API_KEY", getApiKey("KOBIS_API_KEY"))
