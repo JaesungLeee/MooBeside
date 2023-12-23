6622633316d348899052bcadc507bcf300231d35
Jaesung Lee <viki0114@naver.com>
2023-12-23 23:38:32 +0900

    #111 add kakao share dependency

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,9 +7,14 @@ pluginManagement {
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
+    dependencyResolutionManagement {
+        repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+        repositories {
+            google()
+            mavenCentral()
+            gradlePluginPortal()
+            maven(url = "https://devrepo.kakao.com/nexus/content/groups/public/")
+        }
     }
 }
 
