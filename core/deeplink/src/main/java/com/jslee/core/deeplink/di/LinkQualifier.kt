412175bc0f34b88aaba9e3649600fdad6cab3503
Jaesung Lee <viki0114@naver.com>
2024-01-07 10:44:52 +0900

    #111 add link launcher qualifier

---
+++ core/deeplink/src/main/java/com/jslee/core/deeplink/di/LinkQualifier.kt
@@ -0,0 +1,17 @@
+package com.jslee.core.deeplink.di
+
+import javax.inject.Qualifier
+
+/**
+ * MooBeside
+ * @author jaesung
+ * @created 2024/01/07
+ */
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Firebase
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Kakao
