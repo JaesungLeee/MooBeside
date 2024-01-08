0ef6c31393713293aff642db947c388d8f6b5e44
Jaesung Lee <viki0114@naver.com>
2024-01-08 18:58:01 +0900

    #99 add settings detail screen, webview screen

---
+++ presentation/src/main/java/com/jslee/presentation/feature/settings/SettingsWebViewFragment.kt
@@ -0,0 +1,18 @@
+package com.jslee.presentation.feature.settings
+
+import com.jslee.core.ui.base.view.BaseFragment
+import com.jslee.presentation.R
+import com.jslee.presentation.databinding.FragmentSettingsWebViewBinding
+
+/**
+ * MooBeside
+ * @author jaesung
+ * @created 2024/01/08
+ */
+class SettingsWebViewFragment :
+    BaseFragment<FragmentSettingsWebViewBinding>(R.layout.fragment_settings_web_view) {
+
+    override fun initViews() {
+
+    }
+}
