741753521a26cd62b03fd34088e9f02e733a7186
Jaesung Lee <viki0114@naver.com>
2024-01-08 18:58:51 +0900

    #99 navigate with argument

--- presentation/src/main/java/com/jslee/presentation/feature/settings/SettingsWebViewFragment.kt
+++ presentation/src/main/java/com/jslee/presentation/feature/settings/SettingsWebViewFragment.kt
@@ -1,8 +1,11 @@
 package com.jslee.presentation.feature.settings
 
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.jslee.core.ui.base.view.BaseFragment
 import com.jslee.presentation.R
 import com.jslee.presentation.databinding.FragmentSettingsWebViewBinding
+import com.jslee.presentation.feature.settings.model.navigation.NavigationType
 
 /**
  * MooBeside
@@ -12,7 +15,14 @@ import com.jslee.presentation.databinding.FragmentSettingsWebViewBinding
 class SettingsWebViewFragment :
     BaseFragment<FragmentSettingsWebViewBinding>(R.layout.fragment_settings_web_view) {
 
-    override fun initViews() {
+    private val navArgs by navArgs<SettingsWebViewFragmentArgs>()
 
+    override fun initViews() {
+        val type = navArgs.navigationOption.navigationType
+        binding.toolbarTitle = NavigationType.getDescription(type)
+
+        binding.tbSettingsDetail.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 }
