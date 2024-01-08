56642d0d09a7016696df3e7845e193785f89f436
Jaesung Lee <viki0114@naver.com>
2024-01-08 18:54:43 +0900

    #99 add Navigation Path, Type

---
+++ presentation/src/main/java/com/jslee/presentation/feature/settings/model/navigation/NavigationPath.kt
@@ -0,0 +1,10 @@
+package com.jslee.presentation.feature.settings.model.navigation
+
+/**
+ * MooBeside
+ * @author jaesung
+ * @created 2024/01/08
+ */
+enum class NavigationPath {
+    DETAIL, WEB_VIEW;
+}
