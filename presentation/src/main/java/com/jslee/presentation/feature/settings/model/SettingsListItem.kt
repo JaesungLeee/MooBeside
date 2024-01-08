fcf60d60d1b7568ade1a34f816459eeed8c80cb5
Jaesung Lee <viki0114@naver.com>
2024-01-08 16:22:02 +0900

    #99 add options list with view type

---
+++ presentation/src/main/java/com/jslee/presentation/feature/settings/model/SettingsListItem.kt
@@ -0,0 +1,31 @@
+package com.jslee.presentation.feature.settings.model
+
+import com.jslee.core.ui.model.ListItem
+
+/**
+ * MooBeside
+ * @author jaesung
+ * @created 2024/01/08
+ */
+sealed class SettingsListItem(override val viewType: SettingsViewType) : ListItem {
+
+    enum class SettingsViewType {
+        HEADER,
+        OPTION,
+        DIVIDER;
+    }
+
+    data class Header(
+        override val id: Long,
+        val description: String,
+    ) : SettingsListItem(SettingsViewType.HEADER)
+
+    data class Option(
+        override val id: Long,
+        val description: String,
+    ) : SettingsListItem(SettingsViewType.OPTION)
+
+    data class Divider(
+        override val id: Long,
+    ) : SettingsListItem(SettingsViewType.DIVIDER)
+}
