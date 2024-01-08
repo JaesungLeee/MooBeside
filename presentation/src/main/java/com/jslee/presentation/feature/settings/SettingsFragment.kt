5075d0f6dddb03f6f1489adbada033f067753621
Jaesung Lee <viki0114@naver.com>
2024-01-08 16:22:23 +0900

    #99 add options list

--- presentation/src/main/java/com/jslee/presentation/feature/settings/SettingsFragment.kt
+++ presentation/src/main/java/com/jslee/presentation/feature/settings/SettingsFragment.kt
@@ -1,8 +1,11 @@
 package com.jslee.presentation.feature.settings
 
+import androidx.navigation.fragment.findNavController
 import com.jslee.core.ui.base.view.BaseFragment
 import com.jslee.presentation.R
 import com.jslee.presentation.databinding.FragmentSettingsBinding
+import com.jslee.presentation.feature.settings.adapter.SettingsAdapter
+import com.jslee.presentation.feature.settings.model.Settings
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
@@ -12,7 +15,16 @@ import dagger.hilt.android.AndroidEntryPoint
  */
 @AndroidEntryPoint
 class SettingsFragment : BaseFragment<FragmentSettingsBinding>(R.layout.fragment_settings) {
-    override fun initViews() {
 
+    private val settingsAdapter by lazy { SettingsAdapter() }
+    private val optionsList by lazy { Settings.provideOptions() }
+    override fun initViews() {
+        binding.tbSettings.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+
+        binding.rvSettings.adapter = settingsAdapter.also {
+            it.submitList(optionsList)
+        }
     }
 }
---
+++ presentation/src/main/java/com/jslee/presentation/feature/settings/adapter/SettingsAdapter.kt
@@ -0,0 +1,46 @@
+package com.jslee.presentation.feature.settings.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.jslee.core.ui.adapter.MultiViewTypeListAdapter
+import com.jslee.core.ui.base.BaseViewHolder
+import com.jslee.presentation.databinding.ItemDividerBinding
+import com.jslee.presentation.databinding.ItemSettingsHeaderBinding
+import com.jslee.presentation.databinding.ItemSettingsOptionBinding
+import com.jslee.presentation.feature.home.viewholder.divider.DividerViewHolder
+import com.jslee.presentation.feature.settings.model.SettingsListItem
+import com.jslee.presentation.feature.settings.viewholder.SettingsHeaderViewHolder
+import com.jslee.presentation.feature.settings.viewholder.SettingsOptionViewHolder
+
+/**
+ * MooBeside
+ * @author jaesung
+ * @created 2024/01/08
+ */
+class SettingsAdapter :
+    MultiViewTypeListAdapter<SettingsListItem, SettingsListItem.SettingsViewType>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: SettingsListItem.SettingsViewType,
+    ): BaseViewHolder<SettingsListItem> {
+        val inflater = LayoutInflater.from(parent.context)
+
+        return when (viewType) {
+            SettingsListItem.SettingsViewType.HEADER -> {
+                val binding = ItemSettingsHeaderBinding.inflate(inflater, parent, false)
+                SettingsHeaderViewHolder(binding)
+            }
+
+            SettingsListItem.SettingsViewType.OPTION -> {
+                val binding = ItemSettingsOptionBinding.inflate(inflater, parent, false)
+                SettingsOptionViewHolder(binding)
+            }
+
+            SettingsListItem.SettingsViewType.DIVIDER -> {
+                val binding = ItemDividerBinding.inflate(inflater, parent, false)
+                DividerViewHolder(binding)
+            }
+        }
+    }
+}
