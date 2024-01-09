774149ed5ed37ed642c5ce698de460b709d94e73
Jaesung Lee <viki0114@naver.com>
2024-01-09 17:59:48 +0900

    #114 add webview

--- presentation/src/main/java/com/jslee/presentation/feature/settings/SettingsWebViewFragment.kt
+++ presentation/src/main/java/com/jslee/presentation/feature/settings/SettingsWebViewFragment.kt
@@ -1,5 +1,7 @@
 package com.jslee.presentation.feature.settings
 
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.jslee.core.ui.base.view.BaseFragment
@@ -21,8 +23,24 @@ class SettingsWebViewFragment :
         val type = navArgs.navigationOption.navigationType
         binding.toolbarTitle = NavigationType.getDescription(type)
 
-        binding.tbSettingsDetail.setNavigationOnClickListener {
+        binding.tbSettingsWebView.setNavigationOnClickListener {
             findNavController().navigateUp()
+        }
+
+        binding.wvSettings.apply {
+            WebViewClient()
+            WebChromeClient()
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            settings.useWideViewPort = true
+            loadUrl(getUrlFromNavigationType(type))
+        }
+    }
+
+    private fun getUrlFromNavigationType(navType: NavigationType): String {
+        return when (navType) {
+            NavigationType.NOTICE -> "https://jaesungleee.notion.site/d0df58720fad438891c8299aecf4c16e"
+            else -> ""
         }
     }
 }
