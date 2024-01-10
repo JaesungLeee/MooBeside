51e9a554c1d9befa52c10c06828fa490eccfc3fe
Jaesung Lee <viki0114@naver.com>
2024-01-10 18:40:00 +0900

    #117 add cast detail screen

---
+++ presentation/src/main/java/com/jslee/presentation/feature/castdetail/CastDetailFragment.kt
@@ -0,0 +1,17 @@
+package com.jslee.presentation.feature.castdetail
+
+import com.jslee.core.ui.base.view.BaseFragment
+import com.jslee.presentation.R
+import com.jslee.presentation.databinding.FragmentCastDetailBinding
+
+/**
+ * MooBeside
+ * @author jaesung
+ * @created 2024/01/10
+ */
+class CastDetailFragment : BaseFragment<FragmentCastDetailBinding>(R.layout.fragment_cast_detail) {
+
+    override fun initViews() {
+
+    }
+}
