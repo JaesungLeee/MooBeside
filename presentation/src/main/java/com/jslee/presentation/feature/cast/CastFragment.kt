4358e5167719ea125e4ed0aa1d039e79240b7b2a
Jaesung Lee <viki0114@naver.com>
2024-01-10 17:58:44 +0900

    #116 add cast screen

---
+++ presentation/src/main/java/com/jslee/presentation/feature/cast/CastFragment.kt
@@ -0,0 +1,14 @@
+package com.jslee.presentation.feature.cast
+
+import com.jslee.core.ui.base.view.BaseFragment
+import com.jslee.presentation.R
+import com.jslee.presentation.databinding.FragmentCastBinding
+
+/**
+ * MooBeside
+ * @author jaesung
+ * @created 2024/01/10
+ */
+class CastFragment : BaseFragment<FragmentCastBinding>(R.layout.fragment_cast) {
+
+}
