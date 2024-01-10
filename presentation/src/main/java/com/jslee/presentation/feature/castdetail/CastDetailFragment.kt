80b233db36809263d1918b1946dce8e91543e000
Jaesung Lee <viki0114@naver.com>
2024-01-10 18:44:14 +0900

    #117 navigate person id to cast detail screen

--- presentation/src/main/java/com/jslee/presentation/feature/castdetail/CastDetailFragment.kt
+++ presentation/src/main/java/com/jslee/presentation/feature/castdetail/CastDetailFragment.kt
@@ -1,8 +1,10 @@
 package com.jslee.presentation.feature.castdetail
 
+import androidx.navigation.fragment.navArgs
 import com.jslee.core.ui.base.view.BaseFragment
 import com.jslee.presentation.R
 import com.jslee.presentation.databinding.FragmentCastDetailBinding
+import timber.log.Timber
 
 /**
  * MooBeside
@@ -11,7 +13,10 @@ import com.jslee.presentation.databinding.FragmentCastDetailBinding
  */
 class CastDetailFragment : BaseFragment<FragmentCastDetailBinding>(R.layout.fragment_cast_detail) {
 
+    private val navArgs by navArgs<CastDetailFragmentArgs>()
+
     override fun initViews() {
+        Timber.e("${navArgs.personId}")
 
     }
 }
