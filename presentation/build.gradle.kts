79e5fabe177477896b9d355b2df13b13f2411a46
Jaesung Lee <viki0114@naver.com>
2024-01-14 12:01:37 +0900

    #119 add date module

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(project(":core:ui"))
     implementation(project(":core:external"))
     implementation(project(":core:deeplink"))
+    implementation(project(":core:date"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
