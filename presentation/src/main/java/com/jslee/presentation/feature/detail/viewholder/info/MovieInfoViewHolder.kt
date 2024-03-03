3788b6b87b4f6f5cd6d8bc703a3bf55bafa70689
Jaesung Lee <viki0114@naver.com>
2024-03-03 15:44:37 +0900

    #78 add ui logic about expandable overview text when click load more text

--- presentation/src/main/java/com/jslee/presentation/feature/detail/viewholder/info/MovieInfoViewHolder.kt
+++ presentation/src/main/java/com/jslee/presentation/feature/detail/viewholder/info/MovieInfoViewHolder.kt
@@ -1,5 +1,8 @@
 package com.jslee.presentation.feature.detail.viewholder.info
 
+import android.text.TextUtils
+import androidx.core.view.doOnPreDraw
+import androidx.core.view.isVisible
 import com.jslee.core.ui.base.BaseViewHolder
 import com.jslee.core.ui.decoration.CommonItemDecoration
 import com.jslee.core.ui.decoration.LayoutType
@@ -13,6 +16,9 @@ import com.jslee.presentation.feature.detail.model.item.DetailListItem
  * @author jaesung
  * @created 2023/10/21
  */
+
+private const val COLLAPSED_OVERVIEW_LENGTH = 4
+
 class MovieInfoViewHolder(
     private val binding: ItemDetailInfoBinding,
 ) : BaseViewHolder<DetailListItem.MovieInfo>(binding) {
@@ -21,12 +27,39 @@ class MovieInfoViewHolder(
 
     init {
         initMovieInfoList()
+        setLoadMoreTextClickListener()
+        handleMovieOverviewExpandedState()
     }
 
     private fun initMovieInfoList() = with(binding.rvMovieInfo) {
         adapter = movieInfoListAdapter
         val paddingValues = PaddingValues.horizontal(0, 24)
         addItemDecoration(CommonItemDecoration(paddingValues, LayoutType.HORIZONTAL))
+    }
+
+    private fun handleMovieOverviewExpandedState() {
+        binding.tvMovieOverview.doOnPreDraw {
+            val lineCount = binding.tvMovieOverview.lineCount
+            if (lineCount > COLLAPSED_OVERVIEW_LENGTH) {
+                binding.tvMovieOverview.apply {
+                    ellipsize = TextUtils.TruncateAt.END
+                    maxLines = COLLAPSED_OVERVIEW_LENGTH
+                }
+                binding.tvLoadMore.isVisible = true
+            } else {
+                binding.tvLoadMore.isVisible = false
+            }
+        }
+    }
+
+    private fun setLoadMoreTextClickListener() {
+        binding.tvLoadMore.setOnClickListener { loadMoreText ->
+            loadMoreText.isVisible = false
+            binding.tvMovieOverview.apply {
+                ellipsize = null
+                maxLines = Int.MAX_VALUE
+            }
+        }
     }
 
     override fun bindItems(item: DetailListItem.MovieInfo) = with(binding) {
