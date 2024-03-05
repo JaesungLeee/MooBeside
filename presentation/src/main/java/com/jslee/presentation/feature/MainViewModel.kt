cfcc189e46c0cf8d2d1f7eb9c9565be9c51644f9
Jaesung Lee <viki0114@naver.com>
2024-03-05 16:05:34 +0900

    #145 separate visibility responsibility from view to viewmodel

--- presentation/src/main/java/com/jslee/presentation/feature/MainViewModel.kt
+++ presentation/src/main/java/com/jslee/presentation/feature/MainViewModel.kt
@@ -5,7 +5,11 @@ import androidx.lifecycle.ViewModel
 import com.jslee.core.deeplink.DeepLinkLauncher
 import com.jslee.core.deeplink.di.Firebase
 import com.jslee.core.deeplink.di.Kakao
+import com.jslee.presentation.Screen
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 /**
@@ -18,6 +22,18 @@ class MainViewModel @Inject constructor(
     @Firebase private val firebaseLauncher: DeepLinkLauncher,
     @Kakao private val kakaoLauncher: DeepLinkLauncher,
 ) : ViewModel() {
+
+    private val _navBarVisibility: MutableStateFlow<Boolean> = MutableStateFlow(true)
+    val navBarVisibility = _navBarVisibility.asStateFlow()
+
+    fun setCurrentDestination(label: String) {
+        val destination = Screen.fromLabel(label)
+        destination?.let { currentDestination ->
+            _navBarVisibility.update {
+                Screen.getNavigationBarVisibility(currentDestination)
+            }
+        }
+    }
 
     fun extractMovieIdFromDeepLink(
         intent: Intent,
