fd5d522e3d65823c18a8aeaa53c6eaa00956826a
Jaesung Lee <viki0114@naver.com>
2024-03-30 17:03:38 +0900

    #149 modify tmdb api logic and response type

--- data/src/main/java/com/jslee/data/datasource/remote/TmdbRemoteDataSource.kt
+++ data/src/main/java/com/jslee/data/datasource/remote/TmdbRemoteDataSource.kt
@@ -1,6 +1,5 @@
 package com.jslee.data.datasource.remote
 
-import com.jslee.data.UNKNOWN_EXCEPTION
 import com.jslee.data.model.MovieCreditsModel
 import com.jslee.data.model.MovieImagesModel
 import com.jslee.data.model.MovieReleaseInfoModel
@@ -23,84 +22,52 @@ internal class TmdbRemoteDataSource @Inject constructor(
 ) {
 
     suspend fun getSearchMovie(query: String, page: Int): List<TmdbCommonMovieModel> {
-        val response = tmdbService.getSearchMovie(query = query, page = page)
-        return response.pagingResult?.map { it.toDataModel() }.orThrow(response.statusMessage)
+        val response = tmdbService.getSearchMovie(query = query, page = page).getOrThrow()
+        return response.pagingResult?.map { it.toDataModel() }.orEmpty()
     }
 
     suspend fun getPopularMovie(): List<TmdbCommonMovieModel> {
-        val response = tmdbService.getPopularMovie()
-        return response.pagingResult?.map { it.toDataModel() }.orThrow(response.statusMessage)
+        val response = tmdbService.getPopularMovie().getOrThrow()
+        return response.pagingResult?.map { it.toDataModel() }.orEmpty()
     }
 
     suspend fun getNowPlayingMovie(page: Int): List<TmdbCommonMovieModel> {
-        val response = tmdbService.getNowPlayingMovie(page = page)
-        return response.pagingResult?.map { it.toDataModel() }.orThrow(response.statusMessage)
+        val response = tmdbService.getNowPlayingMovie(page = page).getOrThrow()
+        return response.pagingResult?.map { it.toDataModel() }.orEmpty()
     }
 
     suspend fun getUpcomingMovie(page: Int): List<TmdbCommonMovieModel> {
-        val response = tmdbService.getUpcomingMovie(page = page)
-        return response.pagingResult?.map { it.toDataModel() }.orThrow(response.statusMessage)
+        val response = tmdbService.getUpcomingMovie(page = page).getOrThrow()
+        return response.pagingResult?.map { it.toDataModel() }.orEmpty()
     }
 
     suspend fun getMovieReleaseInfo(movieId: Long): List<MovieReleaseInfoModel> {
-        val response = runCatching {
-            tmdbService.getMovieReleaseInfo(movieId = movieId)
-        }.onSuccess { tmdbResponse ->
-            if (tmdbResponse.isSuccess == false) {
-                throw Exception(tmdbResponse.statusMessage ?: UNKNOWN_EXCEPTION)
-            }
-        }.getOrElse {
-            throw Exception(it)
-        }
-
-        return response.pagingResult?.map { it.toDataModel() } ?: emptyList()
+        val response = tmdbService.getMovieReleaseInfo(movieId).getOrThrow()
+        return response.pagingResult?.map { it.toDataModel() }.orEmpty()
     }
 
     suspend fun getMovieCredits(movieId: Long): MovieCreditsModel {
-        val response = runCatching {
-            tmdbService.getMovieCredits(movieId = movieId)
-        }.getOrElse {
-            throw Exception(it)
-        }
-
+        val response = tmdbService.getMovieCredits(movieId).getOrThrow()
         return response.toDataModel()
     }
 
     suspend fun getMovieDetail(movieId: Long): TmdbCommonMovieModel {
-        val response = runCatching {
-            tmdbService.getMovieDetail(movieId = movieId)
-        }.getOrElse {
-            throw Exception(it)
-        }
-
+        val response = tmdbService.getMovieDetail(movieId).getOrThrow()
         return response.toDataModel()
     }
 
     suspend fun getMovieImages(movieId: Long): MovieImagesModel {
-        val response = runCatching {
-            tmdbService.getMovieImages(movieId = movieId)
-        }.getOrElse {
-            throw Exception(it)
-        }
-
+        val response = tmdbService.getMovieImages(movieId).getOrThrow()
         return response.toDataModel()
     }
 
     suspend fun getPerson(personId: Long): PersonInfoModel {
-        val response = runCatching {
-            tmdbService.getPerson(personId)
-        }.getOrElse { throw Exception(it) }
-
+        val response = tmdbService.getPerson(personId).getOrThrow()
         return response.toDataModel()
     }
 
     suspend fun getParticipateMovie(personId: Long): ParticipateCreditsModel {
-        val response = runCatching {
-            tmdbService.getParticipateMovie(personId)
-        }.getOrElse { throw Exception(it) }
-
+        val response = tmdbService.getParticipateMovie(personId).getOrThrow()
         return response.toDataModel()
     }
 }
-
-fun <T> List<T>?.orThrow(message: String?): List<T> = this ?: throw Exception(message)
--- data/src/main/java/com/jslee/data/repository/MovieRepositoryImpl.kt
+++ data/src/main/java/com/jslee/data/repository/MovieRepositoryImpl.kt
@@ -1,13 +1,13 @@
 package com.jslee.data.repository
 
 import androidx.paging.PagingData
-import com.jslee.domain.model.Country
 import com.jslee.data.TMDB_IMAGE_PREFIX
 import com.jslee.data.datasource.remote.KobisRemoteDataSource
 import com.jslee.data.datasource.remote.TmdbRemoteDataSource
 import com.jslee.data.model.toDomain
 import com.jslee.data.paging.extension.createPager
 import com.jslee.data.suspendRunCatching
+import com.jslee.domain.model.Country
 import com.jslee.domain.model.movie.Movie
 import com.jslee.domain.repository.MovieRepository
 import kotlinx.coroutines.flow.Flow
