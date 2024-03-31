5458ae9462d3a67a575f830e1ddd8b49981643a7
Jaesung Lee <viki0114@naver.com>
2024-03-31 17:00:43 +0900

    #154 delete tmdb rate domain model

--- domain/src/main/java/com/jslee/domain/model/movie/Rate.kt
+++ domain/src/main/java/com/jslee/domain/model/movie/Rate.kt
@@ -6,10 +6,6 @@ package com.jslee.domain.model.movie
  * @created 2023/10/04
  */
 data class Rate(
-    val tmdbRate: TmdbRate,
-)
-
-data class TmdbRate(
     val averageVoteRate: Double,
     val voteCount: Int,
 )
