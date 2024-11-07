0964fa152f8863a4828129bead464b9fd8c6fd1b
이재성_Kyle <jaesung.lee@lulumedic.com>
2024-11-07 15:50:45 +0900

    #158 test directory 제거

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -68,7 +68,4 @@ dependencies {
 
     implementation(libs.facebook.shimmer)
     implementation(libs.timber)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 }
