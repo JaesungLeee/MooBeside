9604bff64a26247240f50c3777d9dd5fd1b8c446
JaesungLeee <viki0114@naver.com>
2024-11-07 15:57:20 +0900

    #158 proguard 미사용 파일 제거

--- core/external/build.gradle.kts
+++ core/external/build.gradle.kts
@@ -20,10 +20,6 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
     compileOptions {
--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -25,10 +25,6 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
     compileOptions {
