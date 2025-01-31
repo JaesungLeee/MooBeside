ca84800079b868c5e00ae9fede2a0fe8e9a8c139
JaesungLeee <viki0114@naver.com>
2025-02-01 00:32:03 +0900

    #166 add shape foundation

---
+++ core/designsystem/src/main/java/com/jslee/core/designsystem/core/token/ShapeTokensKey.kt
@@ -0,0 +1,22 @@
+package com.jslee.core.designsystem.core.token
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.graphics.Shape
+import com.jslee.core.designsystem.foundation.shape.fromToken
+import com.jslee.core.designsystem.theme.MooBesideTheme
+
+internal enum class ShapeTokenKey {
+    CornerExtraLarge,
+    CornerLarge,
+    CornerMedium,
+    CornerSmall,
+    CornerExtraSmall,
+    CornerFull,
+    CornerNone,
+}
+
+internal val ShapeTokenKey.value: Shape
+    @Composable
+    @ReadOnlyComposable
+    get() = MooBesideTheme.shapes.fromToken(this)
---
+++ core/designsystem/src/main/java/com/jslee/core/designsystem/foundation/shape/Shapes.kt
@@ -0,0 +1,66 @@
+package com.jslee.core.designsystem.foundation.shape
+
+import androidx.compose.foundation.shape.CornerBasedShape
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Shape
+import com.jslee.core.designsystem.core.token.ShapeTokenKey
+
+@Immutable
+class Shapes(
+    val extraSmall: CornerBasedShape = ShapeDefaults.ExtraSmall,
+    val small: CornerBasedShape = ShapeDefaults.Small,
+    val medium: CornerBasedShape = ShapeDefaults.Medium,
+    val large: CornerBasedShape = ShapeDefaults.Large,
+    val extraLarge: CornerBasedShape = ShapeDefaults.ExtraLarge,
+    val full: CornerBasedShape = ShapeDefaults.Full,
+    val none: Shape = ShapeDefaults.None
+) {
+    fun copy(
+        extraSmall: CornerBasedShape = this.extraSmall,
+        small: CornerBasedShape = this.small,
+        medium: CornerBasedShape = this.medium,
+        large: CornerBasedShape = this.large,
+        extraLarge: CornerBasedShape = this.extraLarge,
+    ): Shapes = Shapes(
+        extraSmall = extraSmall,
+        small = small,
+        medium = medium,
+        large = large,
+        extraLarge = extraLarge,
+    )
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Shapes) return false
+        if (extraSmall != other.extraSmall) return false
+        if (small != other.small) return false
+        if (medium != other.medium) return false
+        if (large != other.large) return false
+        if (extraLarge != other.extraLarge) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = extraSmall.hashCode()
+        result = 31 * result + small.hashCode()
+        result = 31 * result + medium.hashCode()
+        result = 31 * result + large.hashCode()
+        result = 31 * result + extraLarge.hashCode()
+        return result
+    }
+}
+
+internal fun Shapes.fromToken(token: ShapeTokenKey): Shape {
+    return when (token) {
+        ShapeTokenKey.CornerExtraLarge -> extraLarge
+        ShapeTokenKey.CornerLarge -> large
+        ShapeTokenKey.CornerMedium -> medium
+        ShapeTokenKey.CornerSmall -> small
+        ShapeTokenKey.CornerExtraSmall -> extraSmall
+        ShapeTokenKey.CornerFull -> full
+        ShapeTokenKey.CornerNone -> none
+    }
+}
+
+internal val LocalShapes = staticCompositionLocalOf { Shapes() }
