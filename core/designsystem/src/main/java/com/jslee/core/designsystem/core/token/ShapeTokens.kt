ff1da413fd92799e813725c1dab50b226d210883
JaesungLeee <viki0114@naver.com>
2025-02-01 00:32:03 +0900

    #166 add shape foundation

---
+++ core/designsystem/src/main/java/com/jslee/core/designsystem/core/token/ShapeTokens.kt
@@ -0,0 +1,16 @@
+package com.jslee.core.designsystem.core.token
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+
+
+internal object ShapeTokens {
+    val CornerExtraLarge = RoundedCornerShape(20.dp)
+    val CornerLarge = RoundedCornerShape(16.dp)
+    val CornerMedium = RoundedCornerShape(12.dp)
+    val CornerSmall = RoundedCornerShape(8.dp)
+    val CornerExtraSmall = RoundedCornerShape(4.dp)
+    val CornerFull = RoundedCornerShape(50)
+    val CornerNone = RectangleShape
+}
---
+++ core/designsystem/src/main/java/com/jslee/core/designsystem/foundation/shape/ShapeDefaults.kt
@@ -0,0 +1,15 @@
+package com.jslee.core.designsystem.foundation.shape
+
+import androidx.compose.foundation.shape.CornerBasedShape
+import androidx.compose.ui.graphics.Shape
+import com.jslee.core.designsystem.core.token.ShapeTokens
+
+object ShapeDefaults {
+    val ExtraSmall: CornerBasedShape = ShapeTokens.CornerExtraSmall
+    val Small: CornerBasedShape = ShapeTokens.CornerSmall
+    val Medium: CornerBasedShape = ShapeTokens.CornerMedium
+    val Large: CornerBasedShape = ShapeTokens.CornerLarge
+    val ExtraLarge: CornerBasedShape = ShapeTokens.CornerExtraLarge
+    val Full: CornerBasedShape = ShapeTokens.CornerFull
+    val None: Shape = ShapeTokens.CornerNone
+}
