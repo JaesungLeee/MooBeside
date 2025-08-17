e232a372c6f902926e1e083e5b3e2be73571c7ee
JaesungLeee <viki0114@naver.com>
2025-08-17 23:56:48 +0900

    #166 add Tokens

---
+++ core/designsystem/src/main/java/com/jslee/core/designsystem/core/token/DefaultButtonTokens.kt
@@ -0,0 +1,8 @@
+package com.jslee.core.designsystem.core.token
+
+/**
+ * package androidx.compose.material3.tokens.FilledButtonTokens
+ */
+internal object DefaultButtonTokens {
+    val ContainerShape = ShapeTokenKey.CornerSmall
+}
