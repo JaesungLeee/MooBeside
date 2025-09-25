338ef8341b03f17329cb56bf473d7eddf21a944b
JaesungLeee <viki0114@naver.com>
2025-09-25 14:18:12 +0900

    #74 add build-logic module

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,21 @@
+import org.gradle.kotlin.dsl.support.kotlinCompilerOptions
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins { `kotlin-dsl` }
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
+
+dependencies {
+
+}
