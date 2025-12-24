2d41ef4e0ef04a8f93b615fc2f20fa5d70081616
Jaesung Lee <viki0114@naver.com>
2025-12-24 20:40:32 +0900

    #166 add ColorTheme with dark mode

--- core/designsystem/src/main/kotlin/com/jslee/core/designsystem/theme/MooBesideTheme.kt
+++ core/designsystem/src/main/kotlin/com/jslee/core/designsystem/theme/MooBesideTheme.kt
@@ -1,5 +1,6 @@
 package com.jslee.core.designsystem.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -7,11 +8,24 @@ import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.ui.text.TextStyle
 import com.jslee.core.designsystem.foundation.color.ColorScheme
 import com.jslee.core.designsystem.foundation.color.LocalColorScheme
+import com.jslee.core.designsystem.foundation.color.darkColorScheme
+import com.jslee.core.designsystem.foundation.color.lightColorScheme
 import com.jslee.core.designsystem.foundation.shape.LocalShapes
 import com.jslee.core.designsystem.foundation.shape.Shapes
 
 @Composable
-fun MooBesideTheme(
+fun MooBesideAppTheme(
+    content: @Composable () -> Unit,
+) {
+    val colorScheme: ColorScheme = provideColorScheme()
+    MooBesideTheme(
+        colors = colorScheme,
+        content = content,
+    )
+}
+
+@Composable
+private fun MooBesideTheme(
     colors: ColorScheme = MooBesideTheme.colors,
     shapes: Shapes = MooBesideTheme.shapes,
     content: @Composable () -> Unit,
@@ -35,4 +49,13 @@ object MooBesideTheme {
         @Composable
         @ReadOnlyComposable
         get() = LocalShapes.current
+}
+
+
+@Composable
+private fun provideColorScheme(): ColorScheme {
+    return when {
+        isSystemInDarkTheme() -> darkColorScheme()
+        else -> lightColorScheme()
+    }
 }
---
+++ core/designsystem/src/main/kotlin/com/jslee/core/designsystem/token/ColorLightTokens.kt
@@ -0,0 +1,60 @@
+package com.jslee.core.designsystem.token
+
+internal object ColorLightTokens {
+    val foreground: Foreground = Foreground(
+        neutral = ForegroundNeutral(
+            primary = PrimitiveColorToken.Black,
+            secondary = PrimitiveColorToken.Neutral600,
+            tertiary = PrimitiveColorToken.Neutral500,
+            inverse = PrimitiveColorToken.White,
+            static = PrimitiveColorToken.White,
+        ),
+        tint = ForegroundTint(
+            red = PrimitiveColorToken.Red600,
+            orange = PrimitiveColorToken.Amber600,
+            green = PrimitiveColorToken.Green600,
+            blue = PrimitiveColorToken.Blue600,
+        ),
+    )
+
+    val border: Border = Border(
+        neutral = BorderNeutral(
+            primary = PrimitiveColorToken.Neutral950,
+            secondary = PrimitiveColorToken.Neutral500,
+            tertiary = PrimitiveColorToken.Neutral300,
+            transparent = PrimitiveColorToken.Transparent,
+        ),
+        tint = BorderTint(
+            red = PrimitiveColorToken.Red600,
+            orange = PrimitiveColorToken.Amber600,
+            green = PrimitiveColorToken.Green600,
+            blue = PrimitiveColorToken.Blue600,
+        ),
+    )
+
+    val background: Background = Background(
+        bg = Bg(
+            primary = PrimitiveColorToken.White,
+            secondary = PrimitiveColorToken.Neutral100,
+            tertiary = PrimitiveColorToken.Neutral200,
+            dim = PrimitiveColorToken.AlphaBlack600,
+        ),
+        neutral = BackgroundNeutral(
+            primary = PrimitiveColorToken.White,
+            secondary = PrimitiveColorToken.Neutral300,
+            tertiary = PrimitiveColorToken.Neutral200,
+            inverse = PrimitiveColorToken.Neutral950,
+            transparent = PrimitiveColorToken.Transparent,
+        ),
+        tint = BackgroundTint(
+            redBold = PrimitiveColorToken.Red600,
+            redSubtle = PrimitiveColorToken.Red100,
+            orangeBold = PrimitiveColorToken.Amber600,
+            orangeSubtle = PrimitiveColorToken.Amber100,
+            greenBold = PrimitiveColorToken.Green600,
+            greenSubtle = PrimitiveColorToken.Green100,
+            blueBold = PrimitiveColorToken.Blue600,
+            blueSubtle = PrimitiveColorToken.Blue100,
+        ),
+    )
+}
