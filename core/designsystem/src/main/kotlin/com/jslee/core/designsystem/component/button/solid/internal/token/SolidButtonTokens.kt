6b9512fcea089d09428b5d9b4cfc4b1b3670d5b9
Jaesung Lee <viki0114@naver.com>
2025-12-27 15:26:05 +0900

    #166 add SolidButton

---
+++ core/designsystem/src/main/kotlin/com/jslee/core/designsystem/component/button/solid/internal/token/SolidButtonTokens.kt
@@ -0,0 +1,38 @@
+package com.jslee.core.designsystem.component.button.solid.internal.token
+
+import androidx.compose.ui.unit.dp
+import com.jslee.core.designsystem.foundation.color.token.ColorAccessKeyToken
+import com.jslee.core.designsystem.foundation.shape.token.ShapeAccessKeyToken
+
+internal object SolidButtonTokens {
+    val ContainerShape = ShapeAccessKeyToken.CornerMedium
+    val PrimaryContainerColor = ColorAccessKeyToken.SemanticPrimaryNormal
+    val PrimaryLabelColor = ColorAccessKeyToken.SemanticStaticWhite
+    val PrimaryDisabledContainerColor = ColorAccessKeyToken.SemanticInteractionDisable
+    val PrimaryDisabledLabelColor = ColorAccessKeyToken.SemanticLabelAssistive
+    val AssistiveContainerColor = ColorAccessKeyToken.ComponentFillNormal
+    val AssistiveLabelColor = ColorAccessKeyToken.SemanticLabelNeutral
+    val AssistiveDisabledContainerColor = ColorAccessKeyToken.SemanticInteractionDisable
+    val AssistiveDisabledLabelColor = ColorAccessKeyToken.SemanticLabelAssistive
+    val LargeContainerHeight = 48.dp
+    val LargeContainerHorizontalPadding = 28.dp
+    val LargeContainerVerticalPadding = 12.dp
+    val MediumContainerHeight = 40.dp
+    val MediumContainerHorizontalPadding = 20.dp
+    val MediumContainerVerticalPadding = 9.dp
+    val SmallContainerHeight = 32.dp
+    val SmallContainerHorizontalPadding = 14.dp
+    val SmallContainerVerticalPadding = 7.dp
+    val LargeIconSize = 20.dp
+    val MediumIconSize = 18.dp
+    val SmallIconSize = 16.dp
+    val PrimaryIconColor = ColorAccessKeyToken.SemanticStaticWhite
+    val PrimaryDisabledIconColor = ColorAccessKeyToken.SemanticLabelAssistive
+    val AssistiveIconColor = ColorAccessKeyToken.SemanticLabelNeutral
+    val AssistiveDisabledIconColor = ColorAccessKeyToken.SemanticLabelAssistive
+    val IconSpacing = 8.dp
+    val IconButtonContainerShape = ShapeAccessKeyToken.CornerSmall
+    val LargeIconContainerPadding = 12.dp
+    val MediumIconContainerPadding = 10.dp
+    val SmallIconContainerPadding = 7.dp
+}
