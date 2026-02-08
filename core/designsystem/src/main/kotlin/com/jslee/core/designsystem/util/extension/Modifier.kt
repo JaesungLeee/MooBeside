182e106b36d662dba1e4f69288d63d1d8e4421f7
Jaesung Lee <viki0114@naver.com>
2026-02-09 01:41:04 +0900

    #174 fix composed lambda to Node api

--- core/designsystem/src/main/kotlin/com/jslee/core/designsystem/util/extension/Modifier.kt
+++ core/designsystem/src/main/kotlin/com/jslee/core/designsystem/util/extension/Modifier.kt
@@ -2,14 +2,10 @@ package com.jslee.core.designsystem.util.extension
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.semantics.Role
-import com.jslee.core.designsystem.util.MultipleEventsCutter
-import com.jslee.core.designsystem.util.create
+import com.jslee.core.designsystem.util.node.SingleClickableElement
 
 @Composable
 fun Modifier.noRippleClickable(
@@ -33,15 +29,10 @@ fun Modifier.singleClickable(
     onClickLabel: String? = null,
     role: Role? = null,
     onClick: () -> Unit,
-): Modifier = composed {
-    val cutter = remember { MultipleEventsCutter.create() }
-
-    this.clickable(
-        indication = ripple(),
-        interactionSource = interactionSource,
-        enabled = enabled,
-        onClickLabel = onClickLabel,
-        role = role,
-        onClick = { cutter.processEvent(event = onClick) },
-    )
-}
+): Modifier = this then SingleClickableElement(
+    interactionSource = interactionSource,
+    enabled = enabled,
+    onClickLabel = onClickLabel,
+    role = role,
+    onClick = onClick,
+)
