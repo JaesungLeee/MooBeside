97f69b3f687f2051e9a3687e5aef8695f16f2bce
Jaesung Lee <viki0114@naver.com>
2026-02-08 20:01:04 +0900

    #174 add singleClickable with composed lambda

--- core/designsystem/src/main/kotlin/com/jslee/core/designsystem/util/extension/Modifier.kt
+++ core/designsystem/src/main/kotlin/com/jslee/core/designsystem/util/extension/Modifier.kt
@@ -1,11 +1,15 @@
 package com.jslee.core.designsystem.util.extension
 
-import androidx.compose.foundation.Indication
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.semantics.Role
+import com.jslee.core.designsystem.util.MultipleEventsCutter
+import com.jslee.core.designsystem.util.create
 
 @Composable
 fun Modifier.noRippleClickable(
@@ -22,3 +26,22 @@ fun Modifier.noRippleClickable(
     onClick = onClick,
 )
 
+@Composable
+fun Modifier.singleClickable(
+    interactionSource: MutableInteractionSource? = null,
+    enabled: Boolean = true,
+    onClickLabel: String? = null,
+    role: Role? = null,
+    onClick: () -> Unit,
+): Modifier = composed {
+    val cutter = remember { MultipleEventsCutter.create() }
+
+    this.clickable(
+        indication = ripple(),
+        interactionSource = interactionSource,
+        enabled = enabled,
+        onClickLabel = onClickLabel,
+        role = role,
+        onClick = { cutter.processEvent(event = onClick) },
+    )
+}
