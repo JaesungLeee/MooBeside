49ae6e8b80b026755f80cb6e266b4ee7de03747b
Jaesung Lee <viki0114@naver.com>
2026-02-09 22:53:48 +0900

    #162 add architecture

---
+++ core/common/android/src/main/kotlin/com/moobeside/core/common/android/architecture/MviContainer.kt
@@ -0,0 +1,41 @@
+package com.moobeside.core.common.android.architecture
+
+import com.moobeside.core.common.android.architecture.contract.Intent
+import com.moobeside.core.common.android.architecture.contract.SideEffect
+import com.moobeside.core.common.android.architecture.contract.UiState
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class MviContainer<I : Intent, SE : SideEffect, S : UiState>(
+    initialState: S,
+    private val onIntent: MviContext<S, SE>.(I) -> Unit
+) {
+    private val uiStateHolder: UiStateHolder<S> = UiStateHolder(initialState)
+    private val sideEffectEmitter: SideEffectEmitter<SE> = SideEffectEmitter()
+
+    val uiState: StateFlow<S> = uiStateHolder.state
+    val sideEffect: SharedFlow<SE> = sideEffectEmitter.sideEffect
+
+    val mviContext = object : MviContext<S, SE> {
+        override val currentState: S
+            get() = uiState.value
+
+        override fun reduce(action: S.() -> S) {
+            uiStateHolder.reduce(action)
+        }
+
+        override suspend fun postSideEffect(sideEffect: SE) {
+            sideEffectEmitter.emit(sideEffect)
+        }
+    }
+
+    fun handleIntent(intent: I) {
+        onIntent(mviContext, intent)
+    }
+}
+
+interface MviContext<S : UiState, SE : SideEffect> {
+    val currentState: S
+    fun reduce(action: S.() -> S)
+    suspend fun postSideEffect(sideEffect: SE)
+}
