e744e6ed2cf558725caf6f5c9a1585271fbaaae9
Jaesung Lee <viki0114@naver.com>
2026-02-10 20:02:36 +0900

    #173 add bottom navigation bar with top level route

---
+++ app/src/main/java/com/jslee/moobeside/navigation/TopLevelRoute.kt
@@ -0,0 +1,22 @@
+package com.jslee.moobeside.navigation
+
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.jslee.core.designsystem.foundation.icons.MooBesideIconPack
+import com.jslee.core.designsystem.foundation.icons.iconpack.Bookmark
+import com.jslee.core.designsystem.foundation.icons.iconpack.Crown
+import com.jslee.core.designsystem.foundation.icons.iconpack.Ticket
+import com.jslee.core.navigation.BookMark
+import com.jslee.core.navigation.BoxOffice
+import com.jslee.core.navigation.Showing
+
+
+val TOP_LEVEL_ROUTES = mapOf(
+    BoxOffice to NavigationBarItem(label = "박스오피스", icon = MooBesideIconPack.Crown),
+    Showing to NavigationBarItem(label = "상영정보", icon = MooBesideIconPack.Ticket),
+    BookMark to NavigationBarItem(label = "즐겨찾기", icon = MooBesideIconPack.Bookmark)
+)
+
+data class NavigationBarItem(
+    val label: String,
+    val icon: ImageVector,
+)
