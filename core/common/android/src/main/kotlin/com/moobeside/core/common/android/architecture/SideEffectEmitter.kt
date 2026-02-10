fe56da87cb9036454b747ec9ffb818136909139f
Jaesung Lee <viki0114@naver.com>
2026-02-10 13:50:58 +0900

    update to channel

--- core/common/android/src/main/kotlin/com/moobeside/core/common/android/architecture/SideEffectEmitter.kt
+++ core/common/android/src/main/kotlin/com/moobeside/core/common/android/architecture/SideEffectEmitter.kt
@@ -2,18 +2,15 @@ package com.moobeside.core.common.android.architecture
 
 import com.moobeside.core.common.android.architecture.contract.SideEffect
 import kotlinx.coroutines.channels.BufferOverflow
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.receiveAsFlow
 
 internal class SideEffectEmitter<SE : SideEffect> {
-    private val _sideEffect: MutableSharedFlow<SE> = MutableSharedFlow(
-        extraBufferCapacity = 1,
-        onBufferOverflow = BufferOverflow.DROP_OLDEST,
-    )
-    val sideEffect: SharedFlow<SE> = _sideEffect.asSharedFlow()
+    private val _sideEffect: Channel<SE> = Channel(onBufferOverflow = BufferOverflow.DROP_OLDEST)
+    val sideEffect: Flow<SE> = _sideEffect.receiveAsFlow()
 
     suspend fun emit(sideEffect: SE) {
-        _sideEffect.emit(sideEffect)
+        _sideEffect.send(sideEffect)
     }
 }
