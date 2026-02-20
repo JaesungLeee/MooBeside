6348e02e9988c42964b184135fd3ad326e6a983b
Jaesung Lee <viki0114@naver.com>
2026-02-21 00:18:26 +0900

    #173 fix route key

--- core/navigation/src/main/kotlin/com/jslee/core/navigation/RouteKey.kt
+++ core/navigation/src/main/kotlin/com/jslee/core/navigation/RouteKey.kt
@@ -2,9 +2,4 @@ package com.jslee.core.navigation
 
 import androidx.navigation3.runtime.NavKey
 
-@Serializable
-data object BoxOffice : RouteKey
-
-@Serializable
-data object BookMark : RouteKey
 interface RouteKey : NavKey
--- feature/boxoffice/build.gradle.kts
+++ feature/boxoffice/build.gradle.kts
@@ -1,12 +1,27 @@
 plugins {
     alias(libs.plugins.moobeside.android.library.compose)
     alias(libs.plugins.moobeside.android.hilt)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android.namespace = "com.moobeside.feature.boxoffice"
 
 dependencies {
+    implementation(projects.core.common.android)
+    implementation(projects.core.common.kotlin)
+    implementation(projects.core.common.ui)
+    implementation(projects.core.designsystem)
+    implementation(projects.core.domain)
+    implementation(projects.core.model)
+    implementation(projects.core.navigation)
+    implementation(projects.core.logger)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+
+    implementation(libs.androidx.compose.hilt.navigation)
+    implementation(libs.androidx.compose.lifecycle.runtime)
+    implementation(libs.androidx.navigation3.runtime)
+    implementation(libs.androidx.navigation3.ui)
+    implementation(libs.androidx.lifecycle.viewmodel.navigation3)
 }
