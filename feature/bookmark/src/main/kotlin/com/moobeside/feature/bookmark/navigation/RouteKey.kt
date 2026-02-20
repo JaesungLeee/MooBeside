ef67edaf5c9d6c5050fa5b976db2da8e0b889f91
Jaesung Lee <viki0114@naver.com>
2026-02-21 00:18:26 +0900

    #173 fix route key

---
+++ feature/bookmark/src/main/kotlin/com/moobeside/feature/bookmark/navigation/RouteKey.kt
@@ -0,0 +1,7 @@
+package com.moobeside.feature.bookmark.navigation
+
+import com.jslee.core.navigation.RouteKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object BookMark : RouteKey
---
+++ feature/boxoffice/src/main/kotlin/com/moobeside/feature/boxoffice/navigation/RouteKey.kt
@@ -0,0 +1,7 @@
+package com.moobeside.feature.boxoffice.navigation
+
+import com.jslee.core.navigation.RouteKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object BoxOffice : RouteKey
